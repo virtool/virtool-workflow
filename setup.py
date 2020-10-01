2501b4274386f826e27c10101831a839698d3581
BlakeASmith <blake.anthony.smith@gmail.com>
2020-10-01 12:03:20 -0700

    Add setup.py
    Distribute as a pip package with
    entrypoints for accessing the runtime

---
+++ setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+AUTHORS = ["Ian Boyes", "Blake Smith"]
+
+CLASSIFIERS = [
+    "Topic :: Software Development :: Libraries",
+    "Programming Language:: Python:: 3.6",
+]
+
+PACKAGES = find_packages(exclude="tests")
+
+INSTALL_REQUIRES = [
+    git+https://github.com/virtool/virtool-core,
+]
+
+SETUP_REQUIRES = [
+    "pytest",
+    "setuptools>=40.0",
+]
+
+
+ENTRY_POINTS = {
+    "console_scripts": [
+        "virtool_worflow = virtool_workflow._runtime:cli_main",
+    ]
+}
+
+setup(
+    name="virtool_workflow",
+    description="An SDK and runtime for creating Virtool workflows",
+    long_description=Path("README.md").read_text(),
+    url="https://github.com/virtool/virtool-workflow",
+    author=", ".join(AUTHORS),
+    lisense="MIT",
+    platforms="linux",
+    packages=PACKAGES,
+    install_requires=INSTALL_REQUIRES,
+    setup_requries=SETUP_REQUIRES,
+    python_requires=">=3.6",
+    package_dir="virtool_workflow",
+)
