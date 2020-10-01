fd8985bad7a7ecf74aaa8beeba0cb1e295134827
BlakeASmith <blake.anthony.smith@gmail.com>
2020-10-01 15:24:54 -0700

    Add docstring and parameters `progress`, `error`

--- virtool_workflow_runtime/job.py
+++ virtool_workflow_runtime/job.py
@@ -4,6 +4,15 @@ from virtool_workflow import WorkflowExecutionContext, Workflow
 
 @dataclass
 class Job:
+    """
+    A Virtool Job
+
+    :param id: The identifier used in this Job's database entry.
+    :param workflow: The :class:`virtool_workflow.Workflow` being executied.
+    :param context: The :class:`virtool_workflow.WorkflowExecutionContext of the Job.
+    """
     id: str
     workflow: Workflow
     context: WorkflowExecutionContext
+    progress: float = 0.0f
+    error: str
