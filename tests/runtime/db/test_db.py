1c080c2596d9cbbc2d5f0618cc7b7a9873cd5599
Ian Boyes <igboyes@gmail.com>
2020-10-06 15:11:20 -0700

    Fix tests failing when pytest executed in project root

--- tests/runtime/db/test_db.py
+++ tests/runtime/db/test_db.py
@@ -1,14 +1,17 @@
+import sys
 from pathlib import Path
+
 from virtool_workflow_runtime import runtime
 from virtool_workflow_runtime.db import connect
 from virtool_workflow_runtime.discovery import discover_workflow
+
+EXAMPLE_WORKFLOW_PATH = Path(sys.path[0]).joinpath("tests/example_workflow.py")
 
 
 async def test_updates_sent_to_mongo():
     db = connect("test")
     await db.jobs.insert_one({"_id": "1"})
-
-    workflow = discover_workflow(Path("example_workflow.py"))
+    workflow = discover_workflow(EXAMPLE_WORKFLOW_PATH)
     await runtime.execute(workflow, "1", database_name="test")
 
     document = await connect("test").jobs.find_one({"_id": "1"})
