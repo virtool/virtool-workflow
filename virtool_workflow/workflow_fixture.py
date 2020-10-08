2326eb79d0a5a157e11adc525884237495a253b3
Blake Smith <blakeinvictoria@gmail.com>
2020-10-08 12:09:35 -0700

    Add WorkflowFixtureScope
    Fixtures themselves are defined "globally" by WorkflowFixture__subclasses__(). However it makes sense to have the cached instances limited to a particular scope.
    
    WorkflowFixtureScope is a context manager which handles this scoping. Fixtures are injected using
    the WorkflowFixtureScope.inject() method

--- virtool_workflow/workflow_fixture.py
+++ virtool_workflow/workflow_fixture.py
@@ -1,58 +1,81 @@
-from typing import Callable
+from typing import Callable, Any, Optional
 from inspect import signature, iscoroutinefunction, isgeneratorfunction
 from functools import wraps
+from abc import abstractmethod, ABC
+from itertools import chain
+from contextlib import AbstractContextManager
+
+from .workflow import Workflow
 
 
 class InvalidWorkflowFixtureError(ValueError):
     pass
 
 
-class WorkflowFixture(Callable):
-    _instances = {}
-    _generators = []
+class WorkflowFixture(Callable, ABC):
 
-    def __init_subclass__(cls, fixture_func: Callable, *args, **kwargs):
-        super(WorkflowFixture, cls).__init_subclass__(*args, **kwargs)
-        cls.fixture_func = fixture_func
-
-    def __init__(self):
-        self.injected = self.inject(self.__class__.fixture_func)
+    @staticmethod
+    @abstractmethod
+    def fixture_func(*args, **kwargs):
+        pass
 
     def __call__(self, *args, **kwargs):
         return self.fixture_func(*args, **kwargs)
-
-    # noinspection PyTypeChecker
-    def instantiate(self, *args, **kwargs):
-        if isgeneratorfunction(self.__class__.fixture_func):
-            gen = self.injected(*args, **kwargs)
-            fixture_ = next(gen)
-            self._generators.append(gen)
-        else:
-            fixture_ = self.injected(*args, **kwargs)
-        WorkflowFixture._instances[self.__class__.__name__] = fixture_
-        return fixture_
 
     @staticmethod
     def types():
         return {cls.__name__: cls for cls in WorkflowFixture.__subclasses__()}
 
-    @staticmethod
-    def get_fixture(name: str):
-        if name in WorkflowFixture._instances:
-            return WorkflowFixture._instances[name]
+
+class WorkflowFixtureScope(AbstractContextManager):
+
+    def __init__(self):
+        self._instances = {}
+        self._generators = []
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args, **kwargs):
+        self._instances = {}
+        for gen in self._generators:
+            none = next(gen, None)
+            if none is not None:
+                raise InvalidWorkflowFixtureError("Fixture must only yield once")
+        self._generators = []
+
+    def instantiate(self, fixture: WorkflowFixture):
+        injected = self.inject(fixture.__class__.fixture_func)
+
+        if isgeneratorfunction(fixture.__class__.fixture_func):
+            gen = injected()
+            self._generators.append(gen)
+            instance = next(gen)
+        else:
+            instance = injected()
+
+        self._instances[fixture.__class__.__name__] = instance
+        return instance
+
+    def get_fixture(self, name: str):
+        if name in self._instances:
+            return self._instances[name]
 
         fixture_types = WorkflowFixture.types()
         if name in fixture_types:
-            return fixture_types[name]().instantiate()
+            return self.instantiate(fixture_types[name]())
 
-    @staticmethod
-    def inject(func: Callable, **kwargs):
+    def add_instance(self, instance: Any, *names: str):
+        for name in names:
+            self._instances[name] = instance
+
+    def inject(self, func: Callable, **kwargs):
         sig = signature(func)
         fixture_types = WorkflowFixture.types()
 
-        fixtures = {param: WorkflowFixture.get_fixture(param)
+        fixtures = {param: self.get_fixture(param)
                     for param in sig.parameters
-                    if param in fixture_types}
+                    if param in chain(fixture_types, self._instances)}
 
         fixtures.update(kwargs)
 
@@ -69,20 +92,15 @@ class WorkflowFixture(Callable):
 
         return injected
 
-    @staticmethod
-    def clean_instances():
-        WorkflowFixture._instances = {}
-        for gen in WorkflowFixture._generators:
-            none = next(gen, None)
-            if none is not None:
-                raise InvalidWorkflowFixtureError("Fixture must only yield once")
-        WorkflowFixture._generators = []
+    def inject_workflow(self, workflow: Workflow):
+        workflow.on_startup = [self.inject(f) for f in workflow.on_startup]
+        workflow.on_cleanup = [self.inject(f) for f in workflow.on_cleanup]
+        workflow.steps = [self.inject(f) for f in workflow.steps]
 
 
+def workflow_fixture(func: Callable, name: Optional[str] = None):
+    class _Fixture(WorkflowFixture):
+        fixture_func = func
 
-def workflow_fixture(func: Callable):
-    class _Fixture(WorkflowFixture, fixture_func=func):
-        pass
-
-    _Fixture.__name__ = _Fixture.__qualname__ = func.__name__
-    return _Fixture
+    _Fixture.__name__ = _Fixture.__qualname__ = name if name else func.__name__
+    return _Fixture()
