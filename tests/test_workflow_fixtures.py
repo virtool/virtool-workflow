eeefe6e490b8a523edb139681605d4bfbb6554c1
Blake Smith <blakeinvictoria@gmail.com>
2020-10-07 23:48:37 -0700

    Add test for preservation of non-fixture arguments

--- tests/test_workflow_fixtures.py
+++ tests/test_workflow_fixtures.py
@@ -30,3 +30,35 @@ async def test_workflow_fixture_injection_on_async_function():
     with_fixtures_injected = WorkflowFixture.inject(uses_fixture)
     await with_fixtures_injected()
     assert uses_fixture.called
+
+
+async def test_fixtures_used_by_fixtures():
+
+    @workflow_fixture
+    def fixture_using_fixture(my_fixture: str):
+        return f"FIXTURE_USING_{my_fixture}"
+
+    def use_fixture_using_fixture(fixture_using_fixture: str):
+        assert fixture_using_fixture == "FIXTURE_USING_FIXTURE"
+        use_fixture_using_fixture.called = True
+
+    WorkflowFixture.inject(use_fixture_using_fixture)()
+    assert use_fixture_using_fixture.called
+
+
+async def test_preservation_and_injection_of_non_fixture_arguments():
+
+    def use_fixture_and_other_args(arg1: str, my_fixture: str, kwarg=None, other_param=None):
+        assert arg1 == "arg1"
+        assert kwarg is None
+        assert my_fixture == "FIXTURE"
+        assert other_param is not None
+
+    injected = WorkflowFixture.inject(use_fixture_and_other_args, arg1="arg1", other_param="param")
+    injected()
+    injected(kwarg=None, other_param="stuff")
+
+    injected = WorkflowFixture.inject(use_fixture_and_other_args)
+
+    injected(arg1="arg1", other_param="param")
+    injected("arg1", other_param="param")
