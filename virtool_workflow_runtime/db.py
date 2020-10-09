84d03032ba00945313d39b09ca1c769f9bc9deb2
Blake Smith <blakeinvictoria@gmail.com>
2020-10-09 13:11:42 -0700

    Create VirtoolDatabase class
    - Class for database access
    - Uses WorkflowFixture mix-in

--- virtool_workflow_runtime/db.py
+++ virtool_workflow_runtime/db.py
@@ -1,48 +1,48 @@
 """Functions for accessing the Virtool database"""
 from os import getenv
+from typing import Optional
+
+from motor.motor_asyncio import AsyncIOMotorClient
+
 from virtool_core.db.core import DB
 from virtool_core.utils import timestamp
-from motor.motor_asyncio import AsyncIOMotorClient
-from .job import Job
 from virtool_workflow import WorkflowFixture
+from .job import Job
 
 DATABASE_CONNECTION_URL_ENV = "DATABASE_CONNECTION_URL"
 DATABASE_CONNECTION_URL_DEFAULT = "mongodb://localhost:27017"
-DATABASE_NAME = "virtool"
-
-
-def connect(database_name=DATABASE_NAME):
-    client = AsyncIOMotorClient(getenv(DATABASE_CONNECTION_URL_ENV, default=DATABASE_CONNECTION_URL_DEFAULT))
-    return DB(client[database_name], None)
+DEFAULT_DATABASE_NAME = "virtool"
 
 
 class VirtoolDatabase(WorkflowFixture, param_names=["database", "db"]):
 
-    @staticmethod
-    def __fixture__() -> DB:
-        return VirtoolDatabase()
+    def __init__(self, db_name: Optional[str] = None, db_conn_url: Optional[str] = None):
+        if not db_conn_url:
+            db_conn_url = getenv(DATABASE_CONNECTION_URL_ENV, default=DATABASE_CONNECTION_URL_DEFAULT)
+        if not db_name:
+            db_name = DEFAULT_DATABASE_NAME
 
+        self._client = AsyncIOMotorClient(db_conn_url)[db_name]
+        self._db = DB(self._client, None)
 
-
-def set_database_updates(job: Job, database: DB):
-
-    async def _send_update(_, update: str):
-        await database.jobs.update_one({"_id": job.id}, {
-            "$set": {
-                "state": str(job.context.state)
-            },
-            "$push": {
-                "status": {
-                    "state": str(job.context.state),
-                    "stage": job.workflow.steps[job.context.current_step - 1].__name__,
-                    "error": job.context.error,
-                    "progress": job.context.progress,
-                    "update": update,
-                    "timestamp": timestamp(),
+    def set_updates_for_job(self, job: Job):
+        async def _send_update(_, update: str):
+            await self._db.jobs.update_one({"_id": job.id}, {
+                "$set": {
+                    "state": str(job.context.state)
+                },
+                "$push": {
+                    "status": {
+                        "state": str(job.context.state),
+                        "stage": job.workflow.steps[job.context.current_step - 1].__name__,
+                        "error": job.context.error,
+                        "progress": job.context.progress,
+                        "update": update,
+                        "timestamp": timestamp(),
+                    }
                 }
-            }
-        })
+            })
 
-    job.context.on_update(_send_update)
+        job.context.on_update(_send_update)
 
 
--- virtool_workflow_runtime/runtime.py
+++ virtool_workflow_runtime/runtime.py
@@ -2,10 +2,10 @@ from virtool_workflow import Workflow
 from virtool_workflow.workflow_fixture import WorkflowFixtureScope
 import virtool_workflow.execute_workflow
 from .job import Job
-from .db import set_database_updates, connect, DATABASE_NAME
+from .db import set_database_updates, connect, DEFAULT_DATABASE_NAME
 
 
-async def execute(workflow: Workflow, job_id: str, database_name: str = DATABASE_NAME):
+async def execute(workflow: Workflow, job_id: str, database_name: str = DEFAULT_DATABASE_NAME):
     job = Job(job_id, workflow)
     db = connect(database_name)
     set_database_updates(job, db)
