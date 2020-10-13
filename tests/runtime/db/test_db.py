3aec72b726d74ab5cbad0b943610ecced126182b
Blake Smith <blakeinvictoria@gmail.com>
2020-10-13 09:05:52 -0700

    Use `_` in names instead of `__`

--- tests/runtime/db/test_db.py
+++ tests/runtime/db/test_db.py
@@ -2,19 +2,22 @@ import sys
 from pathlib import Path
 
 from virtool_workflow_runtime import runtime
-from virtool_workflow_runtime.db import connect
+from virtool_workflow_runtime.db import VirtoolDatabase
 from virtool_workflow_runtime.discovery import discover_workflow
 
 EXAMPLE_WORKFLOW_PATH = Path(sys.path[0]).joinpath("tests/example_workflow.py")
 
 
 async def test_updates_sent_to_mongo():
-    db = connect("test")
-    await db.jobs.insert_one({"_id": "1"})
+    db = VirtoolDatabase("test")
+    await db._db.jobs.insert_one({"_id": "1"})
     workflow = discover_workflow(EXAMPLE_WORKFLOW_PATH)
-    await runtime.execute(workflow, "1", database_name="test")
 
-    document = await connect("test").jobs.find_one({"_id": "1"})
+    await runtime.execute(workflow, "1")
+
+    document = await db._db.jobs.find_one({"_id": "1"})
+
+    print(document)
 
     updates = [status["update"] for status in document["status"]]
 
