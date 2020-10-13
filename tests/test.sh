48be8149692a0425fb9bf6f3f6a997cadc71ed7b
Blake Smith <blakeinvictoria@gmail.com>
2020-10-13 09:43:56 -0700

    Add redis container to test.sh script
    Run redis in the background during tests

--- tests/test.sh
+++ tests/test.sh
@@ -2,10 +2,21 @@
 # Run pytest along with Mongo and other dependencies
 
 docker pull mongo:4.4.1
+docker pull redis:6.0.8
 
+echo "Starting MongoDB"
 ID=$(docker run -d --network=host mongo)
 
+echo "Starting Redis"
+REDIS=$(docker run -d --network=host redis)
+
+echo "Running pytest"
 pytest . "$@"
 
-docker stop "$ID"
-docker rm "$ID"
+echo "Stopping MongoDB"
+(docker stop "$ID")
+(docker rm "$ID")
+
+echo "Stopping Redis"
+(docker stop "$REDIS")
+(docker rm "$REDIS")
