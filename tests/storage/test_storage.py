010e90b2419b51115e0168341bba48e630eeb24c
Blake Smith <blakeinvictoria@gmail.com>
2020-10-14 16:03:27 -0700

    Add `context_directory` context manager
    Create a new directory and yield it's Path,
    call shutil.rmtree on exit.

---
+++ tests/storage/test_storage.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+from virtool_workflow.storage.paths import context_directory
+
+
+def test_context_directory():
+    with context_directory("foobar") as foobar:
+        with (foobar/"cat").open("w") as cat:
+            cat.write("cat")
+
+        assert foobar.exists()
+        assert (foobar/"cat").exists()
+
+    assert not Path("foobar").exists()
+    assert not Path("foobar/cat").exists()
