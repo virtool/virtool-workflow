2f5a15e3c4677c49bb9b3177959116df25c12311
Blake Smith <blakeinvictoria@gmail.com>
2020-10-15 13:20:33 -0700

    Add type hints

--- virtool_workflow_runtime/db/fixtures.py
+++ virtool_workflow_runtime/db/fixtures.py
@@ -1,22 +1,23 @@
 from virtool_workflow import fixture
 from .db import VirtoolDatabase
+from .db import Collection
 
 
 @fixture
-def caches(database: VirtoolDatabase):
+def caches(database: VirtoolDatabase) -> Collection:
     return database["caches"]
 
 
 @fixture
-def samples(database: VirtoolDatabase):
+def samples(database: VirtoolDatabase) -> Collection:
     return database["samples"]
 
 
 @fixture
-def analyses(database: VirtoolDatabase):
+def analyses(database: VirtoolDatabase) -> Collection:
     return database["analyses"]
 
 
 @fixture
-def jobs(database: VirtoolDatabase):
+def jobs(database: VirtoolDatabase) -> Collection:
     return database["jobs"]
