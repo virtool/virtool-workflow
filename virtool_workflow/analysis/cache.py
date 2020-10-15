e578b8c8a7af2c4dfab1f158e161d4d665851554
Blake Smith <blakeinvictoria@gmail.com>
2020-10-15 13:41:58 -0700

    Create fixture for the database cache
    Fetch cache from database based on sample id and trimming parameters

--- virtool_workflow/analysis/cache.py
+++ virtool_workflow/analysis/cache.py
@@ -1,7 +1,24 @@
+from typing import Dict, Any, Optional
+
 from virtool_workflow import fixture
+from virtool_workflow_runtime.db.fixtures import caches, Collection
+from .trim_parameters import trimming_parameters
+from .analysis_info import sample_id
+from virtool_core.caches.db import calculate_cache_hash
+from virtool_core.db.utils import base_processor
+
+TRIMMING_PROGRAM = "skewer-0.2.2"
 
 
 @fixture
-def cache(job_document):
-    # TODO:
-    pass
+async def cache(
+        trimming_parameters: Dict[str, Any],
+        sample_id: str,
+        caches: Collection,
+) -> Optional[Dict[str, Any]]:
+    return base_processor(caches.find_one({
+        "hash": calculate_cache_hash(trimming_parameters),
+        "missing": False,
+        "program": TRIMMING_PROGRAM,
+        "sample.id": sample_id
+    }))
