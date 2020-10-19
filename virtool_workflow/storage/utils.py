5eb4b35959dd0dfc6b9f66c50552fb086ccd4346
Blake Smith <blakeinvictoria@gmail.com>
2020-10-19 10:47:16 -0700

    Fix `create_cache` parameters

--- virtool_workflow/storage/utils.py
+++ virtool_workflow/storage/utils.py
@@ -1,13 +1,13 @@
 import asyncio
 import shutil
-from typing import List
+from typing import Iterable
 from pathlib import Path
 from virtool_workflow.execute import FunctionExecutor
 
 
 async def copy_paths(
-        sources: List[Path],
-        destinations: List[Path],
+        sources: Iterable[Path],
+        destinations: Iterable[Path],
         run_in_executor: FunctionExecutor):
     coroutines = [
         run_in_executor(shutil.copy, source, destination)
