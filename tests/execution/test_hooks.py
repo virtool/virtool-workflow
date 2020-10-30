e6c58bbab294dccc5f84c222fd7c38b21b0fe1f3
Blake Smith <blakeinvictoria@gmail.com>
2020-10-30 15:02:18 -0700

    Add `temporary_callback` decorator on hook.
    Remove the callback when another hook is triggered. Used to
    remove callbacks that should only be scoped to the execution of
    a single workflow.

--- tests/execution/test_hooks.py
+++ tests/execution/test_hooks.py
@@ -1,6 +1,7 @@
-from virtool_workflow.execution.hooks import hook
-from virtool_workflow import Workflow, WorkflowExecutionContext
-from virtool_workflow.execution.execution_hooks import on_error
+from virtool_workflow.workflow import Workflow
+from virtool_workflow.execution import hooks
+from virtool_workflow.execution.hooks.hooks import hook
+from virtool_workflow.execution.hooks.workflow_hooks import workflow_hook, on_workflow_finish
 
 
 @hook
@@ -19,16 +20,17 @@ async def test_hook():
     assert callback.called
 
 
-async def test_on_error():
+async def test_temporary_callback():
 
-    @on_error.callback
-    def error_callback(error: Exception, workflow: Workflow, context: WorkflowExecutionContext):
+    @example_hook_without_params.temporary_callback(on_workflow_finish)
+    def temporary_callback():
         pass
 
-    await on_error.trigger(ValueError(), Workflow(), WorkflowExecutionContext())
+    assert temporary_callback in example_hook_without_params.callbacks
 
+    await on_workflow_finish.trigger(Workflow(), {})
 
-
-
+    assert temporary_callback not in example_hook_without_params.callbacks
+    assert "remove_callback" not in [f.__name__ for f in example_hook_without_params.callbacks]
 
 
---
+++ virtool_workflow/execution/execution.py
@@ -0,0 +1,9 @@
+from . import workflow_executor
+from ..workflow import Workflow
+from ..fixtures.scope import WorkflowFixtureScope
+
+
+async def execute(workflow: Workflow):
+    with WorkflowFixtureScope() as fixtures:
+        return await workflow_executor.WorkflowExecutor(workflow, fixtures)
+
