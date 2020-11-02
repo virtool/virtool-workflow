16b2850eca662527a8ca17ab56ab52aff44b9d8d
Blake Smith <blakeinvictoria@gmail.com>
2020-11-02 14:05:01 -0800

    Use Hook constructor directly instead of hook decorator.
    Better intellisense completion.

--- virtool_workflow/execution/hooks/__init__.py
+++ virtool_workflow/execution/hooks/__init__.py
@@ -1,33 +1,12 @@
 from typing import Dict, Any
 
-from .hooks import hook
+from .hooks import Hook
 from virtool_workflow.workflow import Workflow
 from virtool_workflow.execution.workflow_executor import WorkflowError, State, WorkflowExecution
 
 
-@hook
-def on_result(workflow: Workflow, result: Dict[str, Any]):
-    pass
-
-
-@hook
-def on_update(workflow: WorkflowExecution, update: str):
-    pass
-
-
-@hook
-def on_workflow_step(executor: WorkflowExecution):
-    pass
-
-
-@hook
-def on_state_change(old_state: State, new_state: State):
-    pass
-
-
-@hook
-def on_error(error: WorkflowError):
-    pass
-
-
-
+on_result = Hook("on_result", [Workflow], Dict[str, Any])
+on_update = Hook("on_update", [WorkflowExecution, str], None)
+on_workflow_step = Hook("on_workflow_step", [WorkflowExecution], None)
+on_state_change = Hook("on_state_change", [State, State], None)
+on_error = Hook("on_error", [WorkflowError], None)
