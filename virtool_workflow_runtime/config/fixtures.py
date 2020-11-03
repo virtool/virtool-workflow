446b5b10e6ef83d69f8ca8a3caac59d0a8f41f95
Blake Smith <blakeinvictoria@gmail.com>
2020-11-03 13:08:02 -0800

    Create fixtures for configuration options

--- virtool_workflow_runtime/config/fixtures.py
+++ virtool_workflow_runtime/config/fixtures.py
@@ -1,9 +1,55 @@
 """Fixtures for getting runtime configuration details."""
 from virtool_workflow import fixture
+from virtool_workflow_runtime.config.configuration import VirtoolConfiguration
 
 
 @fixture
-def number_of_processes() -> int:
+def temp_path_str(config: VirtoolConfiguration) -> str:
+    return config.temp_path
+
+
+@fixture
+def data_path_str(config: VirtoolConfiguration) -> str:
+    return config.data_path
+
+
+@fixture(alt_names=["number_of_processes", "process_limit"])
+def proc(config: VirtoolConfiguration) -> int:
     """The number of allowable processes for the currently executed workflow/job."""
-    # TODO: load virtool config
-    return 3
+    return config.proc
+
+
+@fixture(alt_names=["memory_limit", "RAM_limit"])
+def mem(config: VirtoolConfiguration) -> int:
+    return config.mem
+
+
+@fixture(alt_names=["redis_url"])
+def redis_connection_string(config: VirtoolConfiguration) -> str:
+    return config.redis_connection_string
+
+
+@fixture(alt_names=["sentry_enabled", "use_sentry"])
+def no_sentry(config: VirtoolConfiguration) -> bool:
+    return config.use_sentry
+
+
+@fixture(alt_names=["DEV", "dev_mode"])
+def development_mode(config: VirtoolConfiguration) -> bool:
+    return config.development_mode
+
+
+@fixture(alt_names=["db_name", "mongo_db_name", "database_name"])
+def mongo_database_name(config: VirtoolConfiguration) -> str:
+    return config.mongo_database_name
+
+
+@fixture(alt_names=["db_conn_string", "mongo_conn_string",
+                    "db_conn_str", "mongo_conn_str",
+                    "mongo_connection_string"])
+def db_connection_string(config: VirtoolConfiguration) -> str:
+    return config.mongo_connection_string
+
+
+
+
