c28d2ea08dbb3175a92005aec800353572d3edc6
Blake Smith <blakeinvictoria@gmail.com>
2020-11-03 09:12:54 -0800

    Add `workflow_failure` hook.
    The `on_error` hook gives the opportunity to
    recover from specific failures during a workflow
    step. The `workflow_failure` hook is only triggered
    when a recovery cannot be made, or if there is an error within the executor itself.

--- virtool_workflow/execution/hooks/__init__.py
+++ virtool_workflow/execution/hooks/__init__.py
@@ -5,8 +5,23 @@ from virtool_workflow.workflow import Workflow
 from virtool_workflow.execution.workflow_executor import WorkflowError, State, WorkflowExecution
 
 
-on_result = Hook("on_result", [Workflow], Dict[str, Any])
+on_result = Hook("on_result", [Workflow, Dict[str, Any]], None)
 on_update = Hook("on_update", [WorkflowExecution, str], None)
 on_workflow_step = Hook("on_workflow_step", [WorkflowExecution], None)
 on_state_change = Hook("on_state_change", [State, State], None)
 on_error = Hook("on_error", [WorkflowError], None)
+on_workflow_failure = Hook("on_workflow_finish", [Exception, WorkflowExecution], None)
+on_workflow_finish = Hook("on_workflow_finish", [Workflow], None)
+
+
+@on_workflow_failure
+async def _trigger_finish_from_failure(_, execution):
+    await on_workflow_finish.trigger(execution.workflow)
+
+
+@on_result
+async def _trigger_finish_from_success(workflow, _):
+    await on_workflow_finish.trigger(workflow)
+
+
+
