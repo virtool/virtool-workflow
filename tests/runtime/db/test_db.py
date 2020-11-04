5cbd604b93d497dcedc4aa78be6f4a50bfdb9bc2
Blake Smith <blakeinvictoria@gmail.com>
2020-11-04 12:21:20 -0800

    Use config fixtures for VirtoolDatabase

--- tests/runtime/db/test_db.py
+++ tests/runtime/db/test_db.py
@@ -4,15 +4,21 @@ from pathlib import Path
 from virtool_workflow_runtime import runtime
 from virtool_workflow_runtime.db import VirtoolDatabase
 from virtool_workflow_runtime.discovery import discover_workflow
+from virtool_workflow.fixtures.scope import WorkflowFixtureScope
+from virtool_workflow_runtime.config.fixtures import db_name, db_connection_string
 
 EXAMPLE_WORKFLOW_PATH = Path(sys.path[0]).joinpath("tests/example_workflow.py")
 
 
 async def test_updates_sent_to_mongo():
-    workflow = discover_workflow(EXAMPLE_WORKFLOW_PATH)
+    with WorkflowFixtureScope() as fixtures:
+        name = await fixtures.instantiate(db_name)
+        conn = await fixtures.instantiate(db_connection_string)
 
-    db = VirtoolDatabase()
-    await db._db.jobs.insert_one({"_id": "1"})
+        db = VirtoolDatabase(name, conn)
+        await db._db.jobs.insert_one({"_id": "1"})
+
+    workflow = discover_workflow(EXAMPLE_WORKFLOW_PATH)
 
     await runtime.execute("1", workflow)
 
