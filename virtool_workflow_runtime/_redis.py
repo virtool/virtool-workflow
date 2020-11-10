80a76701081bb26f3d73710b6f5987e61da2b49a
Blake Smith <blakeinvictoria@gmail.com>
2020-11-10 13:26:25 -0800

    Add `redis_list` async generator.
    Read redis lists via an async generator.
    
    Also re-factored `redis_channel` to take an `aioredis.Redis` object instead of a connection string.

--- virtool_workflow_runtime/_redis.py
+++ virtool_workflow_runtime/_redis.py
@@ -5,8 +5,11 @@ from contextlib import asynccontextmanager
 
 import aioredis
 
-VIRTOOL_JOBS_CHANNEL = "channel:dispatch"
 VIRTOOL_JOBS_CANCEL_CHANNEL = "channel:cancel"
+
+
+class JobCancelledViaRedis(RuntimeError):
+    """Raised when a currently executing job is cancelled via the redis cancellation queue."""
 
 
 @asynccontextmanager
@@ -16,7 +19,7 @@ async def connect(address: Optional[str]) -> aioredis.Redis:
 
     :param address: The URL for the redis database, when not provided the value of
             the VIRTOOL_REDIS_ADDRESS environment variable is used.
-    :return Iterator[aioredis.Redis]: A connection to Redis
+    :return Iterator[aioredis.Redis]: A connection to Redis.
     """
     redis_ = await aioredis.create_redis_pool(address, loop=asyncio.get_event_loop())
 
@@ -26,29 +29,32 @@ async def connect(address: Optional[str]) -> aioredis.Redis:
     await redis_.wait_closed()
 
 
-async def job_id_queue(redis_connection: str,
-                       channel: str = VIRTOOL_JOBS_CHANNEL):
+async def redis_channel(redis: aioredis.Redis, channel: str):
+    """Expose redis pub/sub channel as an async generator."""
+    (channel_,) = await redis.subscribe(channel)
+    async for message in channel_.iter(encoding="utf-8"):
+        yield message
+
+
+async def redis_list(redis: aioredis.Redis, list_name: str):
+    """Expose redis list as async generator."""
+    while True:
+        yield await redis.lpop(list_name, encoding="utf-8")
+
+
+async def monitor_cancel(redis: aioredis.Redis, current_job_id: str, task: asyncio.Task) -> asyncio.Task:
     """
-    Exposes the redis jobs channel for Virtool Jobs as an async generator
+    Watch the cancel channel in redis for the current job id.
 
-    :param redis_connection: The URL address of the redis database, if none provided
-        the value of the environment variable VIRTOOL_REDIS_ADDRESS is used
-    :param channel: The redis channel to which job id's are published
-    :return Iterator[str]: The database (mongo) id's for each job to be executed
-    :raise ConnectionRefusedError: When redis is not available at the given URL
+    If the current job id is found, cancel the task.
+
+    :param redis: The redis connection.
+    :param current_job_id: The id of the current job.
+    :param task: The asyncio task in which the job is being executed.
+
+    :return: The asyncio task object after it has been cancelled.
     """
-    async with connect(redis_connection) as redis:
-        (job_ids,) = await redis.subscribe(channel)
-        async for message in job_ids.iter():
-            yield str(message, encoding="utf-8")
-
-
-class JobCancelledViaRedis(RuntimeError):
-    """Raised when a currently executing job is cancelled via the redis cancellation queue."""
-
-
-async def monitor_cancel(redis_connection: str, current_job_id: str, task: asyncio.Task) -> asyncio.Task:
-    async for id_ in job_id_queue(redis_connection, VIRTOOL_JOBS_CANCEL_CHANNEL):
+    async for id_ in redis_channel(redis, VIRTOOL_JOBS_CANCEL_CHANNEL):
         if id_ == current_job_id:
             task.cancel()
             return task
