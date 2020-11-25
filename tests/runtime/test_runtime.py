da8a58bdbfd7909682267d483126f2f513b508e0
Blake Smith <blakeinvictoria@gmail.com>
2020-11-25 14:41:54 -0800

    Properly initialize job document in database before tests.

--- tests/runtime/test_runtime.py
+++ tests/runtime/test_runtime.py
@@ -1,5 +1,11 @@
 from virtool_workflow_runtime import runtime
+from virtool_workflow_runtime.db import VirtoolDatabase
+from virtool_workflow_runtime.config.configuration import db_name, db_connection_string
 
 
 async def test_execute(test_workflow):
+    db = VirtoolDatabase(db_name(), db_connection_string())
+
+    await db["jobs"].insert_one(dict(_id="1", args=dict()))
+
     await runtime.execute("1", test_workflow)
