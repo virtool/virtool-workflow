d8e70b6f8e6b640f6ef782c3c92e4607c33e3b41
Ian Boyes <igboyes@gmail.com>
2020-11-30 13:18:51 -0800

    Add run_subprocess
    Tests are partially broken

---
+++ tests/execution/test_run_subprocess.py
@@ -0,0 +1,86 @@
+import pytest
+import asyncio
+from virtool_workflow.execution.workflow_executor import WorkflowError
+
+from virtool_workflow.execution.run_subprocess import run_subprocess
+from virtool_workflow_runtime import hooks
+
+
+@pytest.fixture
+def bash(tmpdir):
+    sh = """
+    echo "hello world"
+    echo "foo bar"
+    """
+
+    path = tmpdir/"test.sh"
+    path.write(sh)
+
+    return path
+
+
+@pytest.fixture
+def bash_sleep(tmpdir):
+    txt_path = tmpdir/'test.txt'
+
+    sh = f"""
+    sleep 3
+    touch {txt_path}
+    """
+
+    sh_path = tmpdir/"test.sh"
+    sh_path.write(sh)
+
+    return sh_path, txt_path
+
+
+async def test_command_is_called(tmpdir):
+    path = tmpdir / "test.txt"
+    assert not path.isfile()
+
+    func = run_subprocess()
+    await func(["touch", str(path)])
+
+    assert path.isfile()
+
+
+async def test_stdout_is_handled(bash):
+    lines = list()
+
+    async def stdout_handler(line):
+        lines.append(line)
+
+    func = run_subprocess()
+    await func(["bash", str(bash)], stdout_handler=stdout_handler)
+
+    assert lines == [b"hello world\n", b"foo bar\n"]
+
+
+async def test_stderr_is_handled(bash):
+    lines = list()
+
+    async def stderr_handler(line):
+        lines.append(line)
+
+    func = run_subprocess()
+    await func(["bash", "/foo/bar"], stderr_handler=stderr_handler)
+
+    assert lines == [b"bash: /foo/bar: No such file or directory\n"]
+
+
+async def test_command_can_be_terminated(bash_sleep, test_workflow):
+    sh_path, txt_path = bash_sleep
+
+    func = run_subprocess()
+    coro = func(["bash", str(sh_path)])
+
+    await asyncio.sleep(1)
+
+    try:
+        raise Exception("Foo")
+    except Exception as exc:
+        await hooks.on_failure.trigger(WorkflowError(exc, workflow=test_workflow, context=None))
+
+    process = await coro
+
+    assert not txt_path.isfile()
---
+++ virtool_workflow/execution/run_subprocess.py
@@ -0,0 +1,78 @@
+import asyncio
+import asyncio.subprocess
+from logging import getLogger
+from typing import Optional, Callable, Awaitable, List
+
+from virtool_workflow import fixture, hooks
+
+logger = getLogger(__name__)
+
+
+async def watch_pipe(stream: asyncio.StreamReader, handler: Callable[[bytes], Awaitable[None]]):
+    """
+    Watch stdout and stderr streams and pass lines to the #`handler` callback function.
+
+    :param stream: a stdout or stderr file object
+    :param handler: a handler coroutine for output lines
+
+    """
+    while True:
+        line = await stream.readline()
+
+        if not line:
+            return
+
+        await handler(line)
+
+
+async def watch_subprocess(process, stdout_handler, stderr_handler):
+    coros = [
+        watch_pipe(process.stderr, stderr_handler)
+    ]
+
+    if stdout_handler:
+        coros.append(watch_pipe(process.stdout, stdout_handler))
+
+    await asyncio.gather(*coros)
+
+
+@fixture
+def run_subprocess():
+    async def _run_subprocess(
+            command: List[str],
+            stdout_handler: Optional[Callable] = None,
+            stderr_handler=None,
+            env: Optional[dict] = None,
+            cwd: Optional[str] = None
+    ):
+        logger.info(f"Running command in subprocess: {' '.join(command)}")
+
+        stdout = asyncio.subprocess.PIPE if stdout_handler else asyncio.subprocess.DEVNULL
+
+        if stderr_handler:
+            async def _stderr_handler(line):
+                await stderr_handler(line)
+                logger.info(f"STDERR: {line.rstrip()}")
+        else:
+            async def _stderr_handler(line):
+                logger.info(f"STDERR: {line.rstrip()}")
+
+        process = await asyncio.create_subprocess_exec(
+            *(str(arg) for arg in command),
+            stdout=stdout,
+            stderr=asyncio.subprocess.PIPE,
+            env=env,
+            cwd=cwd
+        )
+
+        @hooks.on_workflow_failure
+        def terminate_subprocess():
+            process.terminate()
+
+        await watch_subprocess(process, stdout_handler, _stderr_handler)
+
+        await process.wait()
+
+        return process
+
+    return _run_subprocess
