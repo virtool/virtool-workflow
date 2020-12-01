06dae7b34e55a603a48e959a4870ec88d96091ae
Blake Smith <blakeinvictoria@gmail.com>
2020-12-01 14:56:53 -0800

    Remove direct database access in analysis fixtures.

--- virtool_workflow/db/db.py
+++ virtool_workflow/db/db.py
@@ -1,12 +1,17 @@
 """Central module for database access. """
+from virtool_core.db.bindings import BINDINGS
 import virtool_core.caches.db
+import virtool_core.db.core
 import virtool_core.samples.db
-from virtool_workflow_runtime.db import VirtoolDatabase
-from virtool_workflow_runtime.config.configuration import db_name, db_connection_string
-
+from pathlib import Path
 from typing import Dict, Any, Optional
 
+from virtool_workflow_runtime.config.configuration import db_name, db_connection_string
+from virtool_workflow_runtime.db import VirtoolDatabase
+
 _db = VirtoolDatabase(db_name(), db_connection_string())
+
+COLLECTION_NAMES = [binding.collection_name for binding in BINDINGS]
 
 
 async def fetch_document_by_id(id_: str, collection_name: str) -> Optional[Dict[str, Any]]:
@@ -20,6 +25,7 @@ async def fetch_document_by_id(id_: str, collection_name: str) -> Optional[Dict[
 async def find_document(collection_name: str, *args, **kwargs):
     """Find a document in a database collection based on the given query."""
     return await _db[collection_name].find_one(*args, **kwargs)
+
 
 async def find_cache_document(query: Dict[str, Any]) -> Optional[Dict[str, Any]]:
     """Find a cache document using the given query."""
@@ -61,4 +67,9 @@ async def delete_analysis(analysis_id: str):
 async def recalculate_workflow_tags_for_sample(sample_id):
     """Recalculate the workflow tags for a sample."""
     return await virtool_core.samples.db.recalculate_workflow_tags(_db, sample_id)
+
+
+async def store_result(id_: str, result: Dict[str, Any], collection: str, file_results_location: Path):
+    """Store the result onto the document specified by `id_` in the collection specified by `collection`."""
+    await _db.store_result(id_, _db[collection], result, file_results_location)
 
