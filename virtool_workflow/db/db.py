ba1c4e571b722333e9d53f66998837f7fea26839
Blake Smith <blakeinvictoria@gmail.com>
2020-12-01 11:12:24 -0800

    Use generic `fetch_document_by_id` instead of `fetch_subtraction_document_by_id`.

--- virtool_workflow/db/db.py
+++ virtool_workflow/db/db.py
@@ -2,14 +2,15 @@
 from virtool_workflow_runtime.db import VirtoolDatabase
 from virtool_workflow_runtime.config.configuration import db_name, db_connection_string
 
-from typing import Dict, Any
+from typing import Dict, Any, Optional
 
 _db = VirtoolDatabase(db_name(), db_connection_string())
 
 
-async def fetch_subtraction_document(id_: str) -> Dict[str, Any]:
-    subtraction = await _db["subtractions"].find_one(dict(_id=id_))
+async def fetch_document_by_id(id_: str, collection_name: str) -> Optional[Dict[str, Any]]:
+    """Fetch the document with the given ID from a specific Virtool database collection."""
+    document = _db[collection_name].find_one(dict(_id=id_))
+    if document:
+        document["id"] = document["_id"]
+    return document
 
-    if subtraction:
-        subtraction["id"] = subtraction["_id"]
-        return subtraction
