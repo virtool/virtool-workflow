06283a21807813f6acb7bf8612f786a6c6f19a76
Blake Smith <blakeinvictoria@gmail.com>
2020-12-01 11:41:08 -0800

    Refactor `create_cache` to avoid direct database access.

--- virtool_workflow/analysis/cache.py
+++ virtool_workflow/analysis/cache.py
@@ -1,7 +1,6 @@
 """Create caches of read-prep steps for Virtool analysis workflows."""
 # pylint: disable=redefined-outer-name
 # pylint: disable=too-many-arguments
-import logging
 import shutil
 from pathlib import Path
 from typing import Dict, Any, Optional
@@ -14,9 +13,6 @@ from virtool_workflow.analysis.analysis_info import AnalysisArguments
 from virtool_workflow.execution.run_in_executor import FunctionExecutor
 from virtool_workflow.storage.utils import copy_paths
 from virtool_workflow.db import db
-
-
-logger = logging.getLogger(__name__)
 
 TRIMMING_PROGRAM = "skewer-0.2.2"
 
@@ -64,8 +60,7 @@ async def fetch_cache(
 
 async def create_cache_document(
         analysis_args: AnalysisArguments,
-        trimming_parameters: Dict[str, Any],
-        quality: Dict[str, Any]
+        trimming_parameters: Dict[str, Any]
 ) -> Dict[str, Any]:
     """
     Create a new cache document in the database.
@@ -74,10 +69,9 @@ async def create_cache_document(
 
     :param analysis_args: The AnalysisArguments fixture
     :param trimming_parameters: The trimming parameters (see virtool_workflow.analysis.trimming)
-    :param quality: The parsed fastqc output.
     :return: The cache document which was created.
     """
-    cache = await db.create_cache_document(analysis_args.sample_id, trimming_parameters, analysis_args.paired, quality)
+    cache = await db.create_cache_document(analysis_args.sample_id, trimming_parameters, analysis_args.paired)
 
     await db.update_analysis_with_cache_id(analysis_args.analysis_id, cache["id"])
 
@@ -86,28 +80,40 @@ async def create_cache_document(
 
 async def create_cache(
         fastq: Dict[str, Any],
+        database: VirtoolDatabase,
         analysis_args: AnalysisArguments,
         trimming_parameters: Dict[str, Any],
         trimming_output_path: Path,
         cache_path: Path,
 ):
-    """Cache the trimmed reads and parsed fastqc data."""
-    cache = await create_cache_document(analysis_args, trimming_parameters, quality=fastq)
+    """
+    Cache the trimmed reads and parsed fastqc data.
+
+    :param fastq: The parsed fastqc data produced by #virtool_workflow.analysis.fastqc.parse_fastqc
+        or the #virtool_workflow.analysis.read_paths.parsed_fastqc fixture.
+    """
+    cache = await create_cache_document(database, analysis_args, trimming_parameters)
+
+    await database["caches"].update_one({"_id": cache["id"]}, {"$set": {
+            "quality": fastq
+        }
+    })
 
     shutil.copytree(trimming_output_path, cache_path/cache["id"])
 
 
-async def delete_cache_if_not_ready(cache_id: str, cache_path: Path):
+async def delete_cache_if_not_ready(cache_id: str, caches: Collection, cache_path: Path):
     """
     Delete a cache if it is not ready.
 
     :param cache_id: The database id of the cache document.
+    :param caches: The caches database collection for Virtool.
     :param cache_path: The path to the cache entry which is to be removed.
     """
-    cache = await db.find_document("caches", cache_id, ["ready"])
+    cache = await caches.find_one(cache_id, ["ready"])
 
     if not cache["ready"]:
-        await db.delete_cache(cache_id)
+        await caches.delete_one({"_id": cache_id})
 
         try:
             shutil.rmtree(cache_path)
@@ -115,7 +121,7 @@ async def delete_cache_if_not_ready(cache_id: str, cache_path: Path):
             pass
 
 
-async def delete_analysis(analysis_id: str, analysis_path: Path, sample_id: str):
+async def delete_analysis(analysis_id: str, analysis_path: Path, sample_id: str, database: VirtoolDatabase):
     """
     Delete the analysis associated to `analysis_id`.
 
@@ -123,16 +129,17 @@ async def delete_analysis(analysis_id: str, analysis_path: Path, sample_id: str)
 
     :param analysis_id: The database id of the analysis document.
     :param analysis_path: The virtool analysis path.
-    :param sample_id: The id of the sample associated with this analysis.
+    :param analyses: The analyses database collection.
     """
-    logger.info("Deleting analysis document")
 
-    await db.delete_analysis(analysis_id)
+    analyses = database["analyses"]
+
+    await analyses.delete_one({"_id": analysis_id})
 
     try:
         shutil.rmtree(analysis_path)
     except FileNotFoundError:
         pass
 
-    await db.recalculate_workflow_tags_for_sample(sample_id)
+    await virtool_core.samples.db.recalculate_workflow_tags(database, sample_id)
 
