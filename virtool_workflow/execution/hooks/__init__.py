39bcd5b00d1ce21a5c0d42821e760fd9d8ebd15d
Blake Smith <blakeinvictoria@gmail.com>
2020-12-03 14:41:40 -0800

    Use `WorkflowFixtureHook` wherever possible

--- virtool_workflow/execution/hooks/__init__.py
+++ virtool_workflow/execution/hooks/__init__.py
@@ -1,20 +1,21 @@
 import asyncio
 from concurrent import futures
 from typing import Dict, Any
-from types import SimpleNamespace
 
-from virtool_workflow.execution.workflow_executor import WorkflowError, State, WorkflowExecution
-from virtool_workflow.fixtures.scope import WorkflowFixtureScope
-from virtool_workflow.workflow import Workflow
 from .hooks import Hook
+from .fixture_hooks import WorkflowFixtureHook
+from virtool_workflow.workflow import Workflow
+from virtool_workflow.fixtures.scope import WorkflowFixtureScope
+from virtool_workflow.execution.workflow_executor import WorkflowError, State, WorkflowExecution
 
-on_result = Hook("on_result", [Workflow, Dict[str, Any]], None)
+
+on_result = WorkflowFixtureHook("on_result", [], None)
 """
 Triggered when a workflow has completed and a result is available.
 
 ```python
 @on_result
-async def use_result(workflow: Workflow, result: Dict[str, Any]):
+async def use_result(workflow: Workflow, results: Dict[str, Any]):
     ...
 ```
 
@@ -23,7 +24,7 @@ such the result can be mutated within the callback and that change will be
 reflected in the final result. 
 """
 
-on_update = Hook("on_update", [WorkflowExecution, str], None)
+on_update = WorkflowFixtureHook("on_update", [str], None)
 """
 Triggered when an update is sent from a Workflow. 
 
@@ -37,7 +38,7 @@ async def use_updates(execution: WorkflowExecution, update: str):
 ```
 """
 
-on_workflow_step = Hook("on_workflow_step", [WorkflowExecution], None)
+on_workflow_step = WorkflowFixtureHook("on_workflow_step", [], None)
 """
 Triggered on each workflow step.
 
@@ -48,7 +49,7 @@ async def do_something_on_step(execution: WorkflowExecution):
 ```
 """
 
-on_state_change = Hook("on_state_change", [State, State], None)
+on_state_change = WorkflowFixtureHook("on_state_change", [State, State], None)
 """
 Triggered on a change of state during workflow execution.
 
@@ -59,7 +60,7 @@ async def do_something_on_state_change(old_state, new_state):
 ```
 """
 
-on_error = Hook("on_error", [WorkflowError], None)
+on_error = WorkflowFixtureHook("on_error", [WorkflowError], None)
 """
 Triggered when an exception occurs during a workflow.
 
@@ -70,7 +71,7 @@ async def perform_on_error(error: WorkflowError):
 ```
 """
 
-on_workflow_failure = Hook("on_workflow_finish", [Exception, WorkflowExecution], None)
+on_workflow_failure = WorkflowFixtureHook("on_workflow_finish", [Exception], None)
 """
 Triggered when a workflow fails to complete.
 
@@ -80,7 +81,7 @@ async def perform_on_failure(cause: Exception, execution: WorkflowExecution):
 ```
 """
 
-on_workflow_finish = Hook("on_workflow_finish", [Workflow], None)
+on_workflow_finish = WorkflowFixtureHook("on_workflow_finish", [], None)
 """
 Triggered when a workflow finishes, regardless of it's success.
 
@@ -102,7 +103,7 @@ async def _trigger_finish_from_success(workflow, _):
     await on_workflow_finish.trigger(workflow)
 
 
-on_success = Hook("on_success", parameters=[Workflow, Dict[str, Any]], return_type=None)
+on_success = WorkflowFixtureHook("on_success", parameters=[], return_type=None)
 """
 Triggered when a job completes successfully.
 
@@ -116,7 +117,7 @@ async def perform_on_success(workflow: Workflow, results: Dict[str, Any]):
 """
 
 
-on_failure = Hook("on_failure", parameters=[WorkflowError], return_type=None)
+on_failure = WorkflowFixtureHook("on_failure", parameters=[WorkflowError], return_type=None)
 """
 Triggered when a job fails to complete.
 
@@ -128,35 +129,35 @@ async def perform_on_failure(error: WorkflowError):
 """
 
 
-on_finish = Hook("on_finish", parameters=[Workflow], return_type=None)
+on_finish = WorkflowFixtureHook("on_finish", parameters=[], return_type=None)
 """
 Triggered when a job finishes, regardless of success or failure.
 
 ```python
 @on_finish
-async def perform_on_failure(error: Workflow):
+async def perform_on_finish(workflow: Workflow):
     ...
 ```
 """
 
 
 @on_success
-async def _trigger_on_finish_from_on_success(workflow: Workflow, _):
-    await on_finish.trigger(workflow)
+async def _trigger_on_finish_from_on_success(scope: WorkflowFixtureScope):
+    await on_finish.trigger(scope)
 
 
 @on_failure
-async def _trigger_on_finish_from_on_failure(error: WorkflowError):
-    await on_finish.trigger(error.workflow)
+async def _trigger_on_finish_from_on_failure(error: WorkflowError, scope: WorkflowFixtureScope):
+    await on_finish.trigger(scope, error.workflow)
 
 
-on_cancelled = Hook("on_cancelled", [Workflow, asyncio.CancelledError], None)
+on_cancelled = WorkflowFixtureHook("on_cancelled", [asyncio.CancelledError], None)
 """
 Triggered when a job is cancelled.
 
 ```python
 @on_cancelled
-async def on_cancelled(workflow: Workflow, error: asyncio.CancelledError):
+async def on_cancelled(error: asyncio.CancelledError):
     ...
 ```
 """
@@ -168,10 +169,10 @@ async def _trigger_on_cancelled(error: WorkflowError):
         await on_cancelled.trigger(error.workflow, error.cause)
 
 
-on_load_fixtures = Hook("on_load_fixtures", [WorkflowFixtureScope], return_type=None)
+on_load_fixtures = WorkflowFixtureHook("on_load_fixtures", [WorkflowFixtureScope], return_type=None)
 """
-Triggered after runtime fixtures have been added to the #WorkflowFixtureScope, but 
-before the workflow is executed. 
+Triggered after runtime fixtures have been added to the #WorkflowFixtureScope, but
+before the workflow is executed.
 
 Enables modification or injection of specific fixtures before a workflow is executed.
 
@@ -181,20 +182,6 @@ async def change_fixture_values(fixtures: WorkflowFixtureScope):
     fixtures["some_fixture"] = SOME_VALUE
     await fixtures.get_or_instantiate("name_of_some_other_fixture)
     ...
-```
-"""
-
-on_load_config = Hook("on_load_config", [SimpleNamespace], None)
-"""
-Triggered after the config is loaded from the CLI arguments and environment variables. A SimpleNamespace object
-is provided which has an attribute (sharing the same name as the fixture) for each configuration fixture in
-`virtool_workflow_runtime.config.configuration`. 
-
-```python
-@on_load_config
-def use_config(config: SimpleNamespace):
-    if config.dev_mode:
-        ...
 ```
 """
 
