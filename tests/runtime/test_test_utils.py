0bcfab3915b5b908d759840611a446cd118184a9
Blake Smith <blakeinvictoria@gmail.com>
2020-12-15 10:39:31 -0800

    Create `TestRuntimeEnvironment`.
    Return an instance of this class as the `runtime` fixture.
    
    Includes `execute_function` method for testing functions that use fixtures.

--- tests/runtime/test_test_utils.py
+++ tests/runtime/test_test_utils.py
@@ -4,7 +4,14 @@ from virtool_workflow_runtime.test_utils import runtime
 async def test_runtime_fixture(runtime):
 
     def function_using_fixtures(data_path, temp_path, dev_mode, run_subprocess, run_in_executor):
-        pass
+        return (
+            data_path is not None,
+            temp_path is not None,
+            dev_mode is not None,
+            run_subprocess is not None,
+            run_in_executor is not None,
+        )
 
-    bound = await runtime.bind(function_using_fixtures)
-    bound()
+    results = await runtime.execute_function(function_using_fixtures)
+
+    assert all(results)
--- virtool_workflow_runtime/test_utils.py
+++ virtool_workflow_runtime/test_utils.py
@@ -1,9 +1,24 @@
 import pytest
-from virtool_workflow_runtime.runtime import runtime_scope
+from virtool_workflow_runtime.runtime import runtime_scope, execute
+from typing import Callable
+
+
+class TestRuntimeEnvironment:
+    """Testing harness for Virtool Workflows."""
+    def __init__(self):
+        self.scope = runtime_scope
+
+    async def execute_function(self, func: Callable):
+        """Bind runtime fixtures to a function and execute it."""
+        return (await self.scope.bind(func))()
+
+    execute = execute
 
 
 @pytest.yield_fixture()
 def runtime():
     """The WorkflowFixtureScope which would be used by the runtime."""
-    with runtime_scope as scope:
-        yield scope
+    test_environment = TestRuntimeEnvironment()
+
+    with test_environment.scope:
+        return test_environment
