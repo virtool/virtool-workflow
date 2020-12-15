a7ddee1a02a85ea766dd8e6535e7ea4d1a25328a
Blake Smith <blakeinvictoria@gmail.com>
2020-12-15 09:37:08 -0800

    Create `runtime_scope` which ensures all documented fixtures are loaded.

--- virtool_workflow_runtime/environment.py
+++
@@ -1,6 +0,0 @@
-from typing import Iterable
-from virtool_workflow_runtime.discovery import load_fixtures_from__fixtures__
-
-class WorkflowExecutionEnvironment:
-
-    def __init__(self, fixture_plugins: Iterable[str]):
