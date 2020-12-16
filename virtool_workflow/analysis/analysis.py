c26b6daf1d0dfe4c7279b7ca9b473fd3eaf33417
Blake Smith <blakeinvictoria@gmail.com>
2020-12-15 16:34:52 -0800

    Set result to store in `execute`

--- virtool_workflow/analysis/analysis.py
+++
@@ -1,9 +0,0 @@
-from virtool_workflow import fixture
-from virtool_workflow.abc.db import AbstractDatabase
-from typing import Dict, Any
-
-
-@fixture
-async def analysis(job_args: Dict[str, Any], database: AbstractDatabase):
-    document = await database.fetch_document_by_id(job_args["analysis_id"], "analyses")
-    return document
