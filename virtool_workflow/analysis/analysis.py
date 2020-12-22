a2917d1cd13be0ad43ff1185af50156ca5d29987
Blake Smith <blakeinvictoria@gmail.com>
2020-12-22 10:45:50 -0800

    Add `AbstractFileUploader`.
    Mark files to be uploaded using `.mark()`.
    
    upload the files using `.upload()`

---
+++ virtool_workflow/analysis/analysis.py
@@ -0,0 +1,11 @@
+import virtool_workflow
+import shutil
+
+class Analysis:
+    def __init__(self, _id: str):
+        self._id = _id
+
+    async def upload_file(self, name, ):
+
+@virtool_workflow.fixture
+def analysis():
