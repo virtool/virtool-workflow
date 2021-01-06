a88caf6d4f36db8ef8320eb41281f219e2ddab8e
Blake Smith <blakeinvictoria@gmail.com>
2021-01-06 10:26:12 -0800

    Compute file size and add to database.

--- virtool_workflow/analysis/analysis.py
+++ virtool_workflow/analysis/analysis.py
@@ -1,5 +1,6 @@
 import shutil
 from pathlib import Path
+from virtool_workflow import hooks
 
 import virtool_workflow.abc
 import virtool_workflow.storage.utils
@@ -29,24 +30,22 @@ class AnalysisUploader(virtool_workflow.abc.AbstractFileUploader):
 
     async def upload(self):
         """Move marked files to the :obj:`AnalysisUploader.analysis_path` and create database entries."""
+        source_paths = [file_upload.path for file_upload in self._marks]
+        target_paths = [self.analysis_path/source_path.name for source_path in source_paths]
+
         await virtool_workflow.storage.utils.move_paths(
-            ((file_upload.path, self.analysis_path/file_upload.name) for file_upload in self._marks),
+            zip(source_paths, target_paths),
             self.run_in_executor
         )
 
         await self.analyses_db.update_one(dict(_id=self.analysis_id), {
-            "$push": {
-                "files": {
-                    "$each": {
-                        [{
+            "$set": {
+                "files": [{
                             "name": file_upload.name,
-                            "type": file_upload.format,
-                            "uploaded_at": virtool_core.utils.timestamp(),
-                            "reserved": file_upload.reserved,
-                            "ready": True
-                        } for file_upload in self._marks]
-                    }
-                }
+                            "description": file_upload.description,
+                            "format": file_upload.format,
+                            "size": destination_path.stat().st_size
+                        } for file_upload, destination_path in zip(self._marks, target_paths)]
             }
         })
 
@@ -62,7 +61,10 @@ class Analysis(virtool_workflow.WorkflowFixture, param_name="analysis"):
 
     @staticmethod
     def __fixture__(job_args, run_in_executor, analysis_path: Path, database) -> "Analysis":
-        uploader = AnalysisUploader(analysis_path, run_in_executor, database, )
+        analysis_id = job_args["analysis_id"]
+        uploader = AnalysisUploader(analysis_path, run_in_executor, database, analysis_id)
+
+        hooks.on_result(uploader.upload, once=True)
 
         return Analysis(job_args["analysis_id"], uploader)
 
