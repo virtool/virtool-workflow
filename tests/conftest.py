e94c40011cfb3556f8d4d175cda22e6efe2670ed
Ian Boyes <igboyes@gmail.com>
2021-01-07 13:48:31 -0800

    Add run_in_executor and run_subprocess test fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 pytest_plugins = [
     "tests.fixtures.workflow",
     "tests.fixtures.scope",
+    "tests.fixtures.execution",
     "tests.workflow_with_fixtures",
     "virtool_workflow_runtime.test_utils"
 ]
