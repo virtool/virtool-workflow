1bf8afe73b88edc49f5aa55211c1b741568343da
Blake Smith <blakeinvictoria@gmail.com>
2021-01-11 10:45:41 -0800

    Add `AbstractSubtractionProvider`
    Responsible for operations relating to a subtraction database document.

--- virtool_workflow/abc/db/data_provider.py
+++ virtool_workflow/abc/db/data_provider.py
@@ -1,7 +1,10 @@
 from abc import ABC
 from .analysis import AbstractAnalysisProvider
+from .cache import AbstractCacheProvider
 
 
 class AbstractDataProvider(ABC):
     analysis: AbstractAnalysisProvider
+    caches: AbstractCacheProvider
+
 
