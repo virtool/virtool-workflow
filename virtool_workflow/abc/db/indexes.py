c593ebe583a57cc5f834e5ea1f859c074a7cb7f2
Blake Smith <blakeinvictoria@gmail.com>
2021-01-11 13:38:34 -0800

    Add AbstractIndexProvider
    Responsible for operations involving the index database document.

---
+++ virtool_workflow/abc/db/indexes.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+from typing import Dict, Any
+
+
+class AbstractIndexProvider(ABC):
+
+    @abstractmethod
+    def store_sequence_otu_map(self, sequence_otu_map: Dict[str, Any]):
+        """Store the sequence_otu_map for the current job."""
+        ...
+
+    @abstractmethod
+    def set_ready(self):
+        """Mark the index associated with the current job as ready."""
+        ...
+
+    def set_has_json(self):
+        """Mark that the index associated with the current job has a json representation of the reference available."""
+        ...
