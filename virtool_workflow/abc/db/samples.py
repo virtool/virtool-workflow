95aab7c507b5edb7653db7462925cf10e81b3dd0
Blake Smith <blakeinvictoria@gmail.com>
2021-01-11 11:00:00 -0800

    Add methods to `AbstractSampleProvider`
    - `set_quality`
    - `delete_sample`
    - `set_files`

--- virtool_workflow/abc/db/samples.py
+++ virtool_workflow/abc/db/samples.py
@@ -1,4 +1,6 @@
 from abc import ABC, abstractmethod
+from typing import Dict, Any, List
+from virtool_workflow.uploads.files import DownloadableFileUpload
 
 
 class AbstractSampleProvider(ABC):
@@ -6,5 +8,17 @@ class AbstractSampleProvider(ABC):
     @abstractmethod
     def recalculate_workflow_tags(self):
         """Recalculate workflow tags for samples associated with the current job."""
+        ...
+
+    @abstractmethod
+    def set_quality(self, quality: Dict[str, Any]):
+        """Store the quality of the current sample and mark it as ready."""
+
+    @abstractmethod
+    def delete_sample(self):
+        """Delete the sample associated with the current Job."""
+
+    @abstractmethod
+    def set_files(self, uploads: List[DownloadableFileUpload]):
         ...
 
