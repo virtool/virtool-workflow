79e45d75833a8db1bca7af9188f235fdf2ba6073
Blake Smith <blakeinvictoria@gmail.com>
2021-01-11 11:07:55 -0800

    Add methods to `AbstractSampleProvider`
    - `delete_files`
    - `release_files`

--- virtool_workflow/abc/db/samples.py
+++ virtool_workflow/abc/db/samples.py
@@ -6,19 +6,27 @@ from virtool_workflow.uploads.files import DownloadableFileUpload
 class AbstractSampleProvider(ABC):
 
     @abstractmethod
-    def recalculate_workflow_tags(self):
+    async def recalculate_workflow_tags(self):
         """Recalculate workflow tags for samples associated with the current job."""
         ...
 
     @abstractmethod
-    def set_quality(self, quality: Dict[str, Any]):
+    async def set_quality(self, quality: Dict[str, Any]):
         """Store the quality of the current sample and mark it as ready."""
 
     @abstractmethod
-    def delete_sample(self):
+    async def delete_sample(self):
         """Delete the sample associated with the current Job."""
 
     @abstractmethod
-    def set_files(self, uploads: List[DownloadableFileUpload]):
+    async def set_files(self, uploads: List[DownloadableFileUpload]):
         ...
+
+    @abstractmethod
+    async def delete_files(self):
+        """Delete files associated with the current sample."""
+
+    @abstractmethod
+    async def release_files(self):
+        """Mark each file associated with the current sample as unreserved."""
 
