372685559e168064bd98fe47213a699426c5cc8c
Blake Smith <blakeinvictoria@gmail.com>
2021-01-12 11:41:05 -0800

    Remove `set_cache`. Derive cache from trimming parameters instead.

--- virtool_workflow/abc/providers/analysis.py
+++ virtool_workflow/abc/providers/analysis.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
 from typing import Dict, Any
+
 from virtool_workflow.uploads.files import FileUpload
-from virtool_workflow.abc.providers.cache import CacheEntry
 
 
 class AbstractAnalysisProvider(ABC):
@@ -9,15 +9,10 @@ class AbstractAnalysisProvider(ABC):
     @abstractmethod
     async def store_result(self, result: Dict[str, Any]):
         """
-        Set the result for the current job.
+        Set the result for the current job and mark it as ready.
 
         :param result: A dict representing the results of the current workflow.
         """
-        ...
-
-    @abstractmethod
-    async def set_ready(self, ready=True):
-        """Mark the analysis as "ready", indicating that it's result is ready to be displayed."""
         ...
 
     @abstractmethod
@@ -32,10 +27,5 @@ class AbstractAnalysisProvider(ABC):
     @abstractmethod
     async def delete(self):
         """Delete the analysis for the current job."""
-        ...
-
-    @abstractmethod
-    async def set_cache(self, cache_entry: CacheEntry):
-        """Set the cache to be used if the current analysis job is restarted."""
         ...
 
