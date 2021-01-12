93f02a226a61b7e15ec4f698a981b55cf6bfc05d
Blake Smith <blakeinvictoria@gmail.com>
2021-01-12 11:44:10 -0800

    Remove unneeded abstract methods.

--- virtool_workflow/abc/providers/indexes.py
+++ virtool_workflow/abc/providers/indexes.py
@@ -1,13 +1,7 @@
 from abc import ABC, abstractmethod
-from typing import Dict, Any
 
 
 class AbstractIndexProvider(ABC):
-
-    @abstractmethod
-    def store_sequence_otu_map(self, sequence_otu_map: Dict[str, Any]):
-        """Store the sequence_otu_map for the current job."""
-        ...
 
     @abstractmethod
     def set_has_json(self):
--- virtool_workflow/abc/providers/subtractions.py
+++ virtool_workflow/abc/providers/subtractions.py
@@ -7,12 +7,8 @@ class AbstractSubtractionProvider(ABC):
 
     @abstractmethod
     async def store_count_and_gc(self, count: int, gc: Dict[str, Number]):
-        """Store the count and gc for the current subtraction."""
+        """Store the count and gc for the current subtraction and mark it as ready."""
         ...
-
-    @abstractmethod
-    async def set_ready(self):
-        """Mark the subtraction as ready."""
 
     @abstractmethod
     async def delete(self):
