1f5638205daca2fd1c0293790f49dd9a983ea40a
Blake <blakeinvictoria@gmail.com>
2021-01-14 15:01:37 -0800

    Refactor `WorkflowFixtureScope` to support `FixtureProviders`.

--- virtool_workflow/fixtures/scope.py
+++ virtool_workflow/fixtures/scope.py
@@ -1,43 +1,40 @@
 """Scoping and injection of workflow fixtures."""
+from contextlib import AbstractContextManager
+
 import logging
 import pprint
-from contextlib import AbstractContextManager
+from collections.abc import MutableMapping
 from functools import wraps
 from inspect import isgeneratorfunction, iscoroutinefunction, signature
-from typing import Optional, Dict, Any, Type, Callable, Iterator, Union
+from typing import Any, Callable, Iterator
 
+from virtool_workflow.fixtures.errors import FixtureMultipleYield, FixtureNotAvailable
+from virtool_workflow.fixtures.providers import FixtureProvider, DictProvider, workflow_fixtures, workflow_fixtures_dict
 from virtool_workflow.workflow import Workflow
-from virtool_workflow.fixtures.workflow_fixture import WorkflowFixture
-from virtool_workflow.fixtures.errors import WorkflowFixtureMultipleYield, WorkflowFixtureNotAvailable
 
 logger = logging.getLogger(__name__)
 
 
-class WorkflowFixtureScope(AbstractContextManager):
+class WorkflowFixtureScope(AbstractContextManager, MutableMapping):
     """
     A scope maintaining instances of workflow fixtures.
 
     Fixture instances can be bound to functions using the :func:`.bind()` method.
     """
 
-    def __init__(self,
-                 instances: Optional[Dict[str, Any]] = None,
-                 parent_scope: Optional["WorkflowFixtureScope"] = None):
+    def __init__(self, *providers: FixtureProvider, **instances):
         """
+        :param providers: :class:`FixtureProvider` functions for accessing fixtures. Providers will be checked
+            in the order they are given. The `virtool_workflow.fixtures.providers.workflow_fixture` provider is
+            already included.
 
         :param instances: Any objects to be maintained as instance fixtures.
-            Values in this dictionary will be accessible as fixtures by their key.
-        :param parent_scope: Another WorkflowFixtureScope to inherit instances from.
-            Note that exiting this WorkflowFixtureScope will not invoke __exit__ on the
-            parent scope.
+            Values in this dictionary will be accessible as fixtures by their key. Also note that these
+            fixtures will take precedence over those provided by the elements of :obj:`providers`.
         """
-        self._instances = {"scope": self}
+        self._instances = DictProvider(scope=self, **instances)
+        self._providers = [self._instances, workflow_fixtures, *providers]
         self._generators = []
-
-        if instances:
-            self._instances.update(instances)
-        if parent_scope:
-            self._instances.update(parent_scope._instances)
 
     def __enter__(self):
         """Return this instance when `with` statement is used."""
@@ -53,17 +50,21 @@ class WorkflowFixtureScope(AbstractContextManager):
         """
         logger.debug(f"Closing {WorkflowFixtureScope.__name__} {self}")
         logger.debug("Clearing instances")
-        self._instances = {}
         # return control to the generator fixtures which are still left open
+        self._instances.clear()
         for gen in self._generators:
             logger.debug(f"Returning control to generator fixture {gen}")
             none = next(gen, None)
             if none is not None:
-                raise WorkflowFixtureMultipleYield("Fixture must only yield once")
+                raise FixtureMultipleYield("Fixture must only yield once")
         logger.debug("Clearing generators")
         self._generators = []
 
-    async def instantiate(self, fixture_: Union[WorkflowFixture, Type[WorkflowFixture]]) -> Any:
+    @property
+    def available(self):
+        return {**self._instances, **workflow_fixtures_dict}
+
+    async def instantiate(self, fixture_: Callable) -> Any:
         """
         Create an instance of a fixture.
 
@@ -73,50 +74,62 @@ class WorkflowFixtureScope(AbstractContextManager):
         :return: The instantiated fixture instance.
 
         """
-        __fixture__ = getattr(fixture_.__class__, "__fixture__", None)
-        if not __fixture__:
-            __fixture__ = getattr(fixture_, "__fixture__")
 
-        bound = await self.bind(__fixture__)
+        bound = await self.bind(fixture_)
 
-        if isgeneratorfunction(__fixture__):
+        if isgeneratorfunction(fixture_):
             generator = bound()
             self._generators.append(generator)
             instance = next(generator)
-        elif iscoroutinefunction(__fixture__):
+        elif iscoroutinefunction(fixture_):
             instance = await bound()
         else:
             instance = bound()
 
-        self._instances[fixture_.param_name] = instance
+        self._instances[fixture_.__name__] = instance
 
         logger.debug(f"Instantiated {fixture_} as {instance}")
 
         return instance
 
-    async def get_or_instantiate(self, name: str):
+    def _get_fixture_from_providers(self, name, request_from: Callable = None):
+        """Get the fixture function with the given name from this :class:`WorkflowFixtureScope`'s providers"""
+        for provider in self._providers:
+            fixture = provider(name, request_from)
+            if fixture:
+                return fixture
+
+    async def get_or_instantiate(self, name: str, requested_by: Callable = None):
         """
         Get an instance of the workflow fixture with a given name. If there exists an
         instance cached in this WorkflowFixtureScope it will returned, else a new instance
         will be created and cached.
 
         :param name: The name of the workflow fixture to get
+        :param requested_by: The callable for which the fixture is being fetched. Some :class:`FixtureProviders`
+            will only provide fixtures for specific Callables.
         :return: The workflow fixture instance for this WorkflowFixtureScope
         :raise KeyError: When the given name does not correspond to a defined workflow fixture.
         """
         if name in self._instances:
             return self._instances[name]
 
-        fixture_types = WorkflowFixture.types()
-        if name in fixture_types:
-            return await self.instantiate(fixture_types[name])
+        fixture = self._get_fixture_from_providers(name, requested_by)
+        if fixture:
+            return await self.instantiate(fixture)
 
-        raise KeyError(name, f"{name} is not defined as a workflow fixture")
+        raise KeyError(name, f"{name} is not a fixture within this WorkflowFixtureScope.")
+
+    def __iter__(self):
+        return iter(self._instances)
+
+    def __len__(self) -> int:
+        return len(self._instances)
 
     def __getitem__(self, item: str):
         """Get a fixture instance if one is instantiated within this WorkflowFixtureScope."""
         try:
-            return self._instances.__getitem__(item)
+            return self._instances[item]
         except KeyError as error:
             raise ValueError(
                 f"{error} is not available within this scope.\n"
@@ -125,27 +138,15 @@ class WorkflowFixtureScope(AbstractContextManager):
 
     def __setitem__(self, key: str, value: Any):
         """Add an instance as a fixture with this WorkflowFixtureScope."""
-        return self._instances.__setitem__(key, value)
+        self._instances[key] = value
 
     def __delitem__(self, key: str):
         """Support `del` keyword."""
-        return self._instances.__delitem__(key)
+        del self._instances[key]
 
     def __contains__(self, item):
         """Support `in` operator."""
-        return self._instances.__contains__(item)
-
-    def add_instance(self, instance: Any, *names: str):
-        """
-        Add an instance as a fixture within this WorkflowFixtureScope only. The instance
-        will be bound directly and no subclass of WorkflowFixture will be created.
-
-        :param instance: The instance to use as a workflow fixture
-        :param names: Any names the workflow fixture should be accessible by
-                      (in function parameters)
-        """
-        for name in names:
-            self.__setitem__(name, instance)
+        return item in self._instances
 
     async def bind(self, func: Callable[..., Any], strict: bool = True) -> Callable[[], Any]:
         """
@@ -166,11 +167,11 @@ class WorkflowFixtureScope(AbstractContextManager):
         fixtures = {}
         for param in sig.parameters:
             try:
-                fixtures[param] = await self.get_or_instantiate(param)
+                fixtures[param] = await self.get_or_instantiate(param, requested_by=func)
             except KeyError as key_error:
                 if strict:
                     missing_param = key_error.args[0]
-                    raise WorkflowFixtureNotAvailable(param_name=missing_param, signature=sig, func=func)
+                    raise FixtureNotAvailable(param_name=missing_param, signature=sig, func=func, scope=self)
 
         if iscoroutinefunction(func):
             @wraps(func)
