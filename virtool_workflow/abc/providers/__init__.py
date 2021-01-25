9fbc57f8cef0d43573b7bb8c2d5a850d8eba8a76
Blake <blakeinvictoria@gmail.com>
2021-01-25 15:44:21 -0800

    Add __all__ to `abc.providers `

---
+++ virtool_workflow/abc/providers/__init__.py
@@ -0,0 +1,21 @@
+from .analysis import AbstractAnalysisProvider
+from .cache import AbstractCacheProvider
+from .data_provider import AbstractDataProvider
+from .hmms import AbstractHmmsProvider
+from .indexes import AbstractIndexProvider
+from .otus import AbstractOTUsProvider
+from .references import AbstractReferenceProvider
+from .samples import AbstractSampleProvider
+from .subtractions import AbstractSubtractionProvider
+
+__all__ = [
+    "AbstractAnalysisProvider",
+    "AbstractCacheProvider",
+    "AbstractDataProvider",
+    "AbstractHmmsProvider",
+    "AbstractIndexProvider",
+    "AbstractOTUsProvider",
+    "AbstractReferenceProvider",
+    "AbstractSampleProvider",
+    "AbstractSubtractionProvider"
+]
