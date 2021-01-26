12f2e7a6507a573bdd580e22d972eacdae1e2d78
Blake <blakeinvictoria@gmail.com>
2021-01-25 19:45:35 -0800

    Delete existing instance if `override` is used for a fixture.

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,6 @@ pytest_plugins = [
     "tests.fixtures.workflow",
     "tests.fixtures.scope",
     "tests.fixtures.execution",
-    "tests.workflow_with_fixtures",
     "virtool_workflow.testing"
 ]
 
