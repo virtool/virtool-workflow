a4155b2f98b6d9f6cf271fcee8cce669cebb06a4
Blake <blakeinvictoria@gmail.com>
2021-01-28 15:45:48 -0800

    Refactor `subtractions` test to use data providers.

--- virtool_workflow/analysis/fixtures.py
+++ virtool_workflow/analysis/fixtures.py
@@ -1,6 +1,5 @@
 from virtool_workflow.analysis import paths
 from virtool_workflow.analysis.paths import *
-from virtool_workflow.analysis.references.reference import reference
 from virtool_workflow.analysis.samples.sample import sample, paired
 from virtool_workflow.analysis.subtractions.subtraction import subtractions
 from virtool_workflow.analysis.hmms import hmms
@@ -10,7 +9,6 @@ __all__ = [
    "sample",
    "paired",
    "subtractions",
-   "reference",
    "sample_path",
    "analysis_path",
    "hmms",
