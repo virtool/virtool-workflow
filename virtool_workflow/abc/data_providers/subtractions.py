d1948338d6c4973a3767cf606afe87a695b459f2
Blake <blakeinvictoria@gmail.com>
2021-01-28 15:30:34 -0800

    Refactor `subtractions` fixture to use `subtraction_providers`

--- virtool_workflow/abc/data_providers/subtractions.py
+++ virtool_workflow/abc/data_providers/subtractions.py
@@ -2,8 +2,15 @@ from abc import ABC, abstractmethod
 from typing import Dict
 from numbers import Number
 
+from virtool_workflow.data_model import Subtraction
+
 
 class AbstractSubtractionProvider(ABC):
+
+    @abstractmethod
+    async def fetch_subtraction(self, subtraction_path) -> Subtraction:
+        """Fetch the subtraction associated with this provider."""
+        ...
 
     @abstractmethod
     async def store_count_and_gc(self, count: int, gc: Dict[str, Number]):
