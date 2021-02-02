935fc0730ddd681805d71d4edb4cea7bb517eaa0
Blake <blakeinvictoria@gmail.com>
2021-02-02 10:45:12 -0800

    Implement `set` on `MongoDatabaseCollection`.

--- virtool_workflow/db/mongo.py
+++ virtool_workflow/db/mongo.py
@@ -4,23 +4,6 @@ from virtool_workflow.abc.db import AbstractDatabaseCollection, DocumentUpdater
 from virtool_workflow.db.db import VirtoolDatabase
 
 
-class MongoDocumentUpdater(DocumentUpdater):
-    """A :class:`DocumentUpdater` for MongoDB documents."""
-
-    def __init__(self, id: str, db: AsyncIOMotorCollection):
-        self.id = id
-        self.attrs = {}
-        self._db = db
-
-    def set(self, key, value):
-        self.attrs[key] = value
-
-    async def update(self):
-        return await self._db.update_one(dict(_id=self.id), {
-            "$set": self.attrs
-        })
-
-
 class MongoDatabaseCollection(AbstractDatabaseCollection):
     """A MongoDB database collection."""
 
@@ -34,8 +17,10 @@ class MongoDatabaseCollection(AbstractDatabaseCollection):
         insert_result = await self._db.insert_one(value)
         return insert_result.inserted_id
 
-    def update(self, id: str) -> MongoDocumentUpdater:
-        return MongoDocumentUpdater(id, self._db)
+    async def set(self, id: str, **kwargs):
+        await self._db.update_one(dict(_id=id), {
+            "$set": kwargs
+        })
 
     async def delete(self, id: str):
         await self._db.delete_one(dict(_id=id))
