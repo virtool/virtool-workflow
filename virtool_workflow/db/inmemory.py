18fe98b17c3d7cf7479e2f051de8e062ab753d0d
Blake Smith <blakeinvictoria@gmail.com>
2021-02-02 14:47:01 -0800

    In-memory Implementation of `AbstractDatabaseCollection`. (#105)
    * Implement `AbstractDatabaseCollection` for MongoDB
    * Implement `InMemoryDatabase` class
    * Implement `set, `delete` methods
    * Implement an in memory database collection
    * Remove `MockAnalysisProvider`; will use `AnalysisProvider` implementation with an `InMemoryDatabaseCollection`

---
+++ virtool_workflow/db/inmemory.py
@@ -0,0 +1,37 @@
+from typing import Any, Optional
+from uuid import uuid1
+
+from virtool_workflow.abc.db import AbstractDatabaseCollection
+from virtool_workflow.db.db import VirtoolDatabase
+
+
+class InMemoryDatabaseCollection(AbstractDatabaseCollection):
+    """An in memory database collection for testing."""
+
+    def __init__(self):
+        self._db = {}
+
+    async def get(self, id: str) -> Optional[Any]:
+        if id in self._db:
+            return self._db[id]
+
+    async def set(self, id: str, **kwargs):
+        self._db[id].update(**kwargs)
+
+    async def insert(self, value: Any) -> str:
+        key = uuid1().hex
+        self._db[key] = value
+        return key
+
+    async def delete(self, id: str):
+        del self._db[id]
+
+
+class InMemoryDatabase(VirtoolDatabase):
+    """An in-memory database to use for testing."""
+
+    def __init__(self):
+        super(InMemoryDatabase, self).__init__(
+            *((InMemoryDatabaseCollection(),) * 8)
+        )
+
--- virtool_workflow/testing.py
+++ virtool_workflow/testing.py
@@ -1,30 +1,13 @@
 """Test utilities for Virtool Workflows."""
 import pytest
+
 from typing import Dict, Any, Iterable, Tuple
 from pathlib import Path
 
-from virtool_workflow.abc.data_providers import AbstractAnalysisProvider
+
 from virtool_workflow.analysis.runtime import AnalysisWorkflowRuntime
 from virtool_workflow.data_model import Job
-from virtool_workflow.uploads.files import FileUpload
 from virtool_workflow.storage.paths import context_directory
-
-
-class MockAnalysisProvider(AbstractAnalysisProvider):
-
-    def __init__(self):
-        self.result = None
-        self.uploads = []
-
-    async def store_result(self, result: Dict[str, Any]):
-        self.result = result
-
-    async def store_files(self, uploads: Iterable[Tuple[FileUpload, Path]]):
-        self.uploads = [file_upload for file_upload, _ in uploads]
-
-    async def delete(self):
-        del self.result
-        del self.uploads
 
 
 def testing_data_path():
@@ -36,7 +19,6 @@ def testing_data_path():
 def runtime():
     with AnalysisWorkflowRuntime(
         Job("test_job", {}),
-        analysis_provider=MockAnalysisProvider()
     ) as _runtime:
         _runtime.override("data_path", testing_data_path)
         yield _runtime
@@ -44,5 +26,4 @@ def runtime():
 
 __all__ = [
     "runtime",
-    "MockAnalysisProvider"
 ]
