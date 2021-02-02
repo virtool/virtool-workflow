10761a8172f803d123ba1e23e8a5b608f7391852
Blake <blakeinvictoria@gmail.com>
2021-02-02 11:28:05 -0800

    Remove `MockAnalysisProvider`.
    Will use `AnalysisProvider` implementation with an `InMemoryDatabaseCollection`.

--- virtool_workflow/testing.py
+++ virtool_workflow/testing.py
@@ -1,10 +1,6 @@
 """Test utilities for Virtool Workflows."""
 import pytest
 
-from typing import Dict, Any, Iterable, Tuple
-from pathlib import Path
-
-
 from virtool_workflow.analysis.runtime import AnalysisWorkflowRuntime
 from virtool_workflow.data_model import Job
 from virtool_workflow.storage.paths import context_directory
