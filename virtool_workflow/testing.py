cdede0e2454f72ec70d07906bc295747c4dd0909
Blake <blakeinvictoria@gmail.com>
2021-02-03 15:36:39 -0800

    Add `mock_fixture` decorator.
    replaces a workflow fixture with a new function.

--- virtool_workflow/testing.py
+++ virtool_workflow/testing.py
@@ -1,8 +1,10 @@
 """Test utilities for Virtool Workflows."""
 import pytest
+from typing import Callable, Union
 
 from virtool_workflow.analysis.runtime import AnalysisWorkflowEnvironment
 from virtool_workflow.data_model import Job
+from virtool_workflow.fixtures.scoping import workflow_fixtures
 from virtool_workflow.storage.paths import context_directory
 
 
@@ -18,6 +20,17 @@ def runtime():
     ) as _runtime:
         _runtime.override("data_path", testing_data_path)
         yield _runtime
+
+
+def mock_fixture(fixture: Union[str, Callable]):
+    if not isinstance(fixture, str):
+        fixture = fixture.__name__
+
+    def _add_mock_fixture(func: Callable):
+        workflow_fixtures[fixture] = func
+        return func
+
+    return _add_mock_fixture
 
 
 __all__ = [
