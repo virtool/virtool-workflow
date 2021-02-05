5406ea494d610ea6f876687de3b3cafcca98c1cd
Blake <blakeinvictoria@gmail.com>
2021-02-05 11:29:10 -0800

    Use `blpop` instead of `lpop`.
    Blocks until an item is available.

--- virtool_workflow/abc/data_providers/jobs.py
+++ virtool_workflow/abc/data_providers/jobs.py
@@ -3,7 +3,7 @@ from typing import Protocol
 from virtool_workflow.data_model import Job
 
 
-class JobProvider(Protocol):
+class JobProviderProtocol(Protocol):
     def __call__(self, id_: str) -> Job:
         """Fetch a job with the given id."""
         ...
