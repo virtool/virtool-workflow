93103402a411973e549d58dce34bf3a9a1e2418d
Blake <blakeinvictoria@gmail.com>
2021-02-08 13:08:04 -0800

    Create `loopless_main` pytest fixture.
    Runs `cli.main` without actually processing any jobs.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 pytest_plugins = [
     "tests.virtool_workflow.fixtures.workflow",
     "tests.virtool_workflow.fixtures.execution",
+    "tests.standalone_runner.fixtures",
     "virtool_workflow.testing"
 ]
