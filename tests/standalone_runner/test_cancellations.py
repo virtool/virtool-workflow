6b7497511bc4344e5370dfedb5684665ee48b5c3
Blake <blakeinvictoria@gmail.com>
2021-02-08 11:23:35 -0800

    Test that `watch_cancel` task gets started.

--- tests/standalone_runner/test_cancellations.py
+++ tests/standalone_runner/test_cancellations.py
@@ -1,6 +1,16 @@
+from contextlib import suppress
+
 from virtool_workflow_runtime.cli import main
+from virtool_workflow_runtime.hooks import on_start
 
 
-async def test_monitor_cancel_task_is_running():
-    await main()
-   
+async def test_watch_cancel_task_is_running():
+    @on_start(once=True)
+    def check_watch_cancel_is_running(tasks):
+        assert not tasks["watch_cancel"].done()
+        check_watch_cancel_is_running.called = True
+
+    with suppress(Exception):
+        await main()
+
+    assert check_watch_cancel_is_running.called
