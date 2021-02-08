212cc1c77a44bf1d49a057b35e8b4fa4f59e4b2c
Blake <blakeinvictoria@gmail.com>
2021-02-08 12:22:10 -0800

    Add docstrings for hooks.

--- virtool_workflow_runtime/hooks.py
+++ virtool_workflow_runtime/hooks.py
@@ -2,12 +2,28 @@ from virtool_workflow.execution.hooks import on_load_config
 from virtool_workflow.execution.hooks.fixture_hooks import FixtureHook
 
 on_init = FixtureHook("on_init", [], None)
+"""
+Triggered before the configuration is loaded by the CLI. 
+
+Intended for initialization tasks that don't depend on any configuration variables.
+"""
 
 on_start = FixtureHook("on_start", [], None)
+"""Triggered when the runtime starts, after the configuration is loaded."""
 
 on_redis_connect = FixtureHook("on_redis_connect", [], None)
+"""
+Triggered when a connection to redis is established. 
+
+The :class:`aioredis.Redis` object is available as a fixture `redis`.
+"""
 
 on_exit = FixtureHook("on_exit", [], None)
+"""
+Triggered before the process exists.
+
+If the process is exiting due to an exception, the `error` fixture will hold the exception.
+"""
 
 on_job_cancelled = FixtureHook("on_job_cancelled", [str], None)
 
