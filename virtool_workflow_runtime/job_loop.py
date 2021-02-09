82939adfe34ae5680584cfd0a7e374ad0b27fd9d
Blake <blakeinvictoria@gmail.com>
2021-02-09 14:10:40 -0800

    Stop any running containers on exit.

--- virtool_workflow_runtime/job_loop.py
+++ virtool_workflow_runtime/job_loop.py
@@ -1,18 +1,13 @@
 import logging
 
 from virtool_workflow.data_model import Job
-from virtool_workflow_runtime.hooks import on_init
+from virtool_workflow_runtime.hooks import on_exit
 
 logger = logging.getLogger(__name__)
 
 
-@on_init
-def instantiate_job_container_dict(scope):
-    scope["job_containers"] = {}
-
-
-async def process_job(job: Job, image: str, docker, job_containers):
-    job_containers[job._id] = docker.containers.run(image, detach=True)
+async def process_job(job: Job, image: str, command: str, docker, job_containers):
+    job_containers[job] = docker.containers.run(image, command, detach=True)
 
 
 async def job_loop(jobs, workflow_to_docker_image, scope):
@@ -22,3 +17,9 @@ async def job_loop(jobs, workflow_to_docker_image, scope):
         logger.debug(f"Processing job {job}")
         image = workflow_to_docker_image[job.task]
         await _process_job(job, image)
+
+
+@on_exit
+def stop_all_containers(job_containers):
+    for container in job_containers.values():
+        container.stop()
