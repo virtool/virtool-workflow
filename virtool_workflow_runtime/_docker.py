078190213827ec913195b08de0dd5ef9739edead
Blake <blakeinvictoria@gmail.com>
2021-02-09 10:34:02 -0800

    Connect to an existing docker swarm.

--- virtool_workflow_runtime/_docker.py
+++ virtool_workflow_runtime/_docker.py
@@ -1,6 +1,11 @@
 import docker
+import logging
+from typing import List
 
-from virtool_workflow_runtime.hooks import on_load_config, on_docker_connect
+from virtool_workflow.fixtures.scope import FixtureScope
+from virtool_workflow_runtime.hooks import on_load_config, on_docker_connect, on_join_swarm
+
+logger = logging.getLogger(__name__)
 
 
 @on_load_config
@@ -13,3 +18,26 @@ async def create_docker_client(docker_daemon_url, scope):
     scope["docker"].ping()
 
     await on_docker_connect.trigger(scope)
+
+
+@on_docker_connect
+async def join_swarm(docker: docker.DockerClient,
+                     swarm_manager_nodes: List[str],
+                     swarm_join_token: str,
+                     swarm_listen_address: str,
+                     swarm_advertise_address: str,
+                     swarm_data_path_address: str,
+                     scope: FixtureScope):
+    if swarm_manager_nodes:
+        logger.info("Joining docker swarm.")
+        docker.swarm.join(
+            remote_addrs=swarm_manager_nodes,
+            join_token=swarm_join_token,
+            listen_addr=swarm_listen_address,
+            advertise_addr=swarm_advertise_address,
+            data_path_addr=swarm_data_path_address,
+        )
+
+        await on_join_swarm.trigger(scope)
+
+    logger.info("No docker swarm configured, using a single docker engine only.")
