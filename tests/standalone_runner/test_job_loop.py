493fd4769d20289efe8f87705b0f6baa8419e4ee
Blake <blakeinvictoria@gmail.com>
2021-02-10 11:19:54 -0800

    Test that containers are stopped when a job is cancelled.

--- tests/standalone_runner/test_job_loop.py
+++ tests/standalone_runner/test_job_loop.py
@@ -10,18 +10,18 @@ from virtool_workflow_runtime.hooks import on_init, on_docker_container_exit, on
 TEST_IMAGE_MAP_JSON = Path(__file__).parent / "default_images.json"
 
 
-@on_init
-def set_job_provider(scope):
-    scope["job_provider"] = lambda id_: Job(id_, {}, task="test")
+@on_docker_container_exit
+def job_container_exit(_, tasks):
+    job_container_exit.called = True
+    tasks["job_loop"].cancel()
 
 
 async def test_containers_are_started_when_jobs_are_given_to_redis():
-    @on_docker_container_exit
-    def job_container_exit(_, tasks):
-        job_container_exit.called = True
-        tasks["job_loop"].cancel()
+    @on_init(once=True)
+    def set_job_provider(scope):
+        scope["job_provider"] = lambda id_: Job(id_, {}, task="test")
 
-    @on_redis_connect
+    @on_redis_connect(once=True)
     async def submit_a_job(redis, redis_job_list_name):
         await redis.lpush(redis_job_list_name, "test_job")
 
@@ -29,3 +29,27 @@ async def test_containers_are_started_when_jobs_are_given_to_redis():
         await main(workflow_to_docker_image=TEST_IMAGE_MAP_JSON)
 
     assert job_container_exit.called
+
+
+async def test_containers_are_stopped_when_jobs_are_cancelled():
+    @on_init(once=True)
+    def set_job_provider(scope):
+        scope["job_provider"] = lambda id_: Job(id_, {}, task="ubuntu")
+
+    @on_redis_connect(once=True)
+    async def submit_a_job_and_cancel(redis, redis_job_list_name, redis_cancel_list_name):
+        await redis.lpush(redis_job_list_name, "test_job")
+
+        async def _wait_and_send_cancel():
+            await asyncio.sleep(1)
+            await redis.publish(redis_cancel_list_name, "test_job")
+
+        asyncio.create_task(_wait_and_send_cancel())
+
+        submit_a_job_and_cancel.called = True
+
+    with suppress(asyncio.CancelledError):
+        await main(workflow_to_docker_image=TEST_IMAGE_MAP_JSON)
+
+    assert job_container_exit.called
+    assert submit_a_job_and_cancel.called
