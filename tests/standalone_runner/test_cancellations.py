523063e7b1f7677af244121359fd94e7570b6055
Blake <blakeinvictoria@gmail.com>
2021-02-10 11:31:51 -0800

    Remove `running_jobs` dict.
    Jobs are not asyncio tasks, but running docker containers, so there is no need to keep track of them in order to cancel.

--- tests/standalone_runner/test_cancellations.py
+++ tests/standalone_runner/test_cancellations.py
@@ -22,8 +22,6 @@ async def test_watch_cancel_task_is_running(loopless_main):
 async def test_running_jobs_get_cancelled(loopless_main):
     @on_start(once=True)
     async def start_a_mock_job_and_send_cancel_signal_to_redis(redis, running_jobs, redis_cancel_list_name):
-        running_jobs["1"] = asyncio.create_task(asyncio.sleep(10000))
-
         # Allow some time for the `watch_cancel` task to prepare before publishing.
         await asyncio.sleep(0.5)
 
@@ -33,9 +31,8 @@ async def test_running_jobs_get_cancelled(loopless_main):
         await asyncio.sleep(0.5)
 
     @on_job_cancelled(once=True)
-    def check_cancelled(_id, running_jobs):
+    def check_cancelled(_id):
         assert _id == "1"
-        assert _id not in running_jobs
         check_cancelled.called = True
 
     await loopless_main()
