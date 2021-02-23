2b78bc733730dfffdbd7c986da3f7ddec7c4fed9
Blake <blakeinvictoria@gmail.com>
2021-02-23 12:34:19 -0800

    Add `http_client` fixture.

--- virtool_workflow/api/client.py
+++ virtool_workflow/api/client.py
@@ -1,1 +1,9 @@
-from virtool_workflow.fixtures.scoping import
+import aiohttp
+
+from virtool_workflow.fixtures.scoping import api_scope
+
+
+@api_scope.fixture
+async def http_client():
+    async with aiohttp.ClientSession() as session:
+        yield session
