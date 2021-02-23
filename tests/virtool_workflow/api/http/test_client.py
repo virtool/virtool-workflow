772d3a168de583d4270ee97435f6f411633d6d65
Blake <blakeinvictoria@gmail.com>
2021-02-23 12:34:19 -0800

    Add `http_client` fixture.

---
+++ tests/virtool_workflow/api/http/test_client.py
@@ -0,0 +1,2 @@
+def test_http_client():
+    assert False
--- virtool_workflow/api/jobs.py
+++ virtool_workflow/api/jobs.py
@@ -1,2 +1,6 @@
-def acquire(job_id: str):
+from virtool_workflow.fixtures.scoping import api_scope
+
+
+@api_scope.fixture
+def job_provider(job_id: str):
     ...
