5e34e5600f1158562d91df184549d07b1fea565d
Blake <blakeinvictoria@gmail.com>
2021-02-23 16:21:08 -0800

    Support async generator fixtures.

--- tests/virtool_workflow/api/http/test_client.py
+++ tests/virtool_workflow/api/http/test_client.py
@@ -1,2 +1,13 @@
-def test_http_client():
-    assert False
+import aiohttp
+
+from virtool_workflow.api.scope import api_scope
+
+
+async def test_http_client():
+    client = await api_scope.get_or_instantiate("http_client")
+
+    assert isinstance(client, aiohttp.ClientSession)
+
+    await api_scope.close()
+
+    assert client.closed
--- virtool_workflow/fixtures/scoping.py
+++ virtool_workflow/fixtures/scoping.py
@@ -5,12 +5,8 @@ from virtool_workflow.fixtures.workflow_fixture import workflow_fixtures
 workflow_scope = FixtureScope(workflow_fixtures)
 """The :class:`FixtureScope` to be used for workflow runs."""
 
-api_scope = FixtureScope()
-"""The :class:`FixtureScope` which contains the `aiohttp` client."""
-
 
 @hooks.on_finish
-def _close_scopes():
+async def _close_scopes():
     """Close :class:`FixtureScope` instances when the workflow finishes."""
-    workflow_scope.close()
-    api_scope.close()
+    await workflow_scope.close()
