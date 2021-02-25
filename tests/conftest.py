ef728cbc22019194d8392f54673c68b6753f26c6
Blake <blakeinvictoria@gmail.com>
2021-02-25 10:44:05 -0800

    Add `--db-connection-string` option for tests.

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,5 +2,10 @@ pytest_plugins = [
     "tests.virtool_workflow.fixtures.workflow",
     "tests.virtool_workflow.fixtures.execution",
     "tests.standalone_runner.fixtures",
+    "tests.fixtures.db"
     "virtool_workflow.testing"
 ]
+
+
+def pytest_addoption(parser):
+    parser.add_option("--db-connection-string", action="store", default="mongodb://localhost:27017")
