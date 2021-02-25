d9e59931068efcc119dff660e5fe6c9aca504e45
Blake <blakeinvictoria@gmail.com>
2021-02-25 10:56:46 -0800

    Add `connect_to_db` fixture which returns a function for connecting to mongo.
    Avoids `attached to different event loop` error.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,18 @@
+import pytest
+
 pytest_plugins = [
     "tests.virtool_workflow.fixtures.workflow",
     "tests.virtool_workflow.fixtures.execution",
     "tests.standalone_runner.fixtures",
-    "tests.fixtures.db"
-    "virtool_workflow.testing"
+    "tests.fixtures.db",
+    "virtool_workflow.testing",
 ]
 
 
 def pytest_addoption(parser):
-    parser.add_option("--db-connection-string", action="store", default="mongodb://localhost:27017")
+    parser.addoption("--db-connection-string", action="store", default="mongodb://localhost:27017")
+
+
+@pytest.fixture
+def db_connection_string(pytestconfig):
+    return pytestconfig.getoption("db_connection_string")
