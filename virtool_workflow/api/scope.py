6aad0290d4c51f6df432726a18f0fbeff14db3d1
Blake <blakeinvictoria@gmail.com>
2021-02-25 13:23:36 -0800

    Add `install_as_pytest_fixtures`.
    Creates pytest fixtures from a FixtureGroup.

--- virtool_workflow/api/scope.py
+++ virtool_workflow/api/scope.py
@@ -1,11 +1,13 @@
 from .client import http_client
+from .jobs import acquire_job
 from .. import FixtureScope
 from ..config.fixtures import jobs_api_url
 from ..fixtures import FixtureGroup
 
 api_fixtures = FixtureGroup(
-    jobs_api_url=jobs_api_url,
-    http_client=http_client,
+    jobs_api_url,
+    http_client,
+    acquire_job
 )
 
 api_scope = FixtureScope(api_fixtures)
