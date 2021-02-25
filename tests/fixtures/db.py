68c5bdfc935c7c075e242e52e194b912353b9f0a
Blake <blakeinvictoria@gmail.com>
2021-02-25 10:46:10 -0800

    Add `dbi` test fixture.

--- tests/fixtures/db.py
+++ tests/fixtures/db.py
@@ -1,7 +1,9 @@
 import pytest
+from motor.motor_asyncio import AsyncIOMotorClient
 from virtool_core.db.core import DB
 
 
 @pytest.fixture
-def dbi():
-    return DB()
+def dbi(db_connection_string: str):
+    client = AsyncIOMotorClient(db_connection_string)
+    return DB(client["test"], None)
