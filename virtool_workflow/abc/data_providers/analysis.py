28f01281b59ea6983da93043931dd3cc83cd3c9e
Blake <blakeinvictoria@gmail.com>
2021-02-26 15:23:49 -0800

    Change `AbstractAnalysisProvider` to more directly reflect the jobs API docs.

--- virtool_workflow/abc/data_providers/analysis.py
+++ virtool_workflow/abc/data_providers/analysis.py
@@ -1,11 +1,23 @@
 from abc import ABC, abstractmethod
-from pathlib import Path
-from typing import Dict, Any, Tuple, Iterable
+from typing import Dict, Any
 
-from virtool_workflow.uploads.files import FileUpload
+from virtool_workflow.data_model.analysis import Analysis
+from virtool_workflow.data_model.files import AnalysisFile
 
 
 class AbstractAnalysisProvider(ABC):
+
+    @abstractmethod
+    async def get(self) -> Analysis:
+        ...
+
+    @abstractmethod
+    async def upload(self, file: AnalysisFile):
+        ...
+
+    @abstractmethod
+    async def download(self, file_id):
+        ...
 
     @abstractmethod
     async def store_result(self, result: Dict[str, Any]):
@@ -13,15 +25,6 @@ class AbstractAnalysisProvider(ABC):
         Set the result for the current job and mark it as ready.
 
         :param result: A dict representing the results of the current workflow.
-        """
-        ...
-
-    @abstractmethod
-    async def store_files(self, uploads: Iterable[Tuple[FileUpload, Path]]):
-        """
-        Register that a file is available under the `data_path`.
-
-        :param uploads: An Iterable of :class:`FileUpload` and destination path tuples.
         """
         ...
 
