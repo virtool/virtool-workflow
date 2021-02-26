79f3c3b52433ef91c5eb1f6a91fc1b99ea3bf2ea
Blake <blakeinvictoria@gmail.com>
2021-02-26 10:45:14 -0800

    Use test `http` fixture.

--- tests/virtool_workflow/runtime/test_workflow_runtime.py
+++ tests/virtool_workflow/runtime/test_workflow_runtime.py
@@ -1,9 +1,11 @@
 from pathlib import Path
 
 from virtool_workflow import runtime
+from virtool_workflow.api.scope import api_fixtures
 
 WORKFLOW_FILE = Path(__file__).parent / "workflow.py"
 
 
-async def test_start(http):
-    await runtime.start(dev_mode=True, workflow_file_path=WORKFLOW_FILE, job_api_url="")
+async def test_start(http, jobs_api_url):
+    api_fixtures["http"] = lambda: http
+    await runtime.start(dev_mode=True, workflow_file_path=WORKFLOW_FILE, jobs_api_url="/api")
