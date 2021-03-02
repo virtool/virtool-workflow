2a7451ce7a0e826fa326523df51382718514f838
Blake <blakeinvictoria@gmail.com>
2021-03-02 13:15:44 -0800

    Add `raise_server_errors` context manager to handle raising errors for different response status codes.

--- virtool_workflow/api/errors.py
+++ virtool_workflow/api/errors.py
@@ -1,3 +1,8 @@
+from contextlib import contextmanager
+
+from aiohttp import ContentTypeError
+
+
 class JobAlreadyAcquired(Exception):
     def __init__(self, job_id: str):
         super(JobAlreadyAcquired, self).__init__(f"Job {job_id} is has already been acquired.")
@@ -17,3 +22,31 @@ class NotFound(KeyError):
 
 class AlreadyFinalized(Exception):
     ...
+
+
+@contextmanager
+def raise_server_errors(response,
+                        on_409=AlreadyFinalized,
+                        on_404=NotFound,
+                        on_403=InsufficientJobRights,
+                        on_other=JobsAPIServerError):
+    if 200 <= response.status < 300:
+        yield
+    else:
+
+        try:
+            response_json = await response.json()
+            response_message = response_json["message"]
+        except ContentTypeError:
+            response_message = await response.text()
+        except KeyError:
+            response_message = response_json
+
+        if response.status == 404:
+            raise on_404(response_message)
+        elif response.status == 409:
+            raise on_409(response_message)
+        elif response.status == 403:
+            raise on_403(response_message)
+        else:
+            raise on_other(response_message)
