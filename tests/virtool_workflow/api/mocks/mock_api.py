ec2c524af8e4146e26675acc957fe65d38dc743a
Blake <blakeinvictoria@gmail.com>
2021-03-08 13:03:36 -0800

    Remove print statements.

--- tests/virtool_workflow/api/mocks/mock_api.py
+++ tests/virtool_workflow/api/mocks/mock_api.py
@@ -1,7 +1,5 @@
 from . import mock_analysis_routes, mock_index_routes, mock_job_routes, mock_subtraction_routes
 
-print(type(mock_analysis_routes.mock_routes))
-
 mock_routes = [mock_analysis_routes.mock_routes,
                mock_job_routes.mock_routes,
                mock_index_routes.mock_routes,
