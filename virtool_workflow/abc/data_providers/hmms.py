d0f8e3ee417a058e769d917e806c9884ea06d1e6
Blake Smith <blakeinvictoria@gmail.com>
2021-03-11 11:10:38 -0800

    Update `AbstractHmmsProvider.`

--- virtool_workflow/abc/data_providers/hmms.py
+++ virtool_workflow/abc/data_providers/hmms.py
@@ -1,10 +1,23 @@
 from abc import ABC, abstractmethod
+from pathlib import Path
 from typing import List
+
 from virtool_workflow.data_model import HMM
 
 
 class AbstractHmmsProvider(ABC):
 
     @abstractmethod
+    async def get(self, hmm_id: str):
+        """Get the HMM annotation with the given ID."""
+        ...
+
+    @abstractmethod
     async def hmm_list(self) -> List[HMM]:
+        """Get a list of all HMM annotations."""
+        ...
+
+    @abstractmethod
+    async def get_profiles(self) -> Path:
+        """Get the profiles.hmm file."""
         ...
---
+++ virtool_workflow/api/hmm.py
@@ -0,0 +1,4 @@
+from virtool_workflow.abc.data_providers import AbstractHmmsProvider
+
+
+class HmmsProvider(AbstractHmmsProvider):
