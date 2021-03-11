5fec96f1d97695ae840754dd3454adbcdf8fad39
Blake Smith <blakeinvictoria@gmail.com>
2021-03-11 10:37:17 -0800

    Add `mock_hmm_routes` module.

--- tests/virtool_workflow/api/mocks/mock_api.py
+++ tests/virtool_workflow/api/mocks/mock_api.py
@@ -1,8 +1,7 @@
-from . import mock_analysis_routes, mock_index_routes, mock_job_routes, mock_subtraction_routes, mock_sample_routes
-
+from . import mock_analysis_routes, mock_index_routes, mock_job_routes, mock_subtraction_routes, mock_hmm_routes
 
 mock_routes = [mock_analysis_routes.mock_routes,
                mock_job_routes.mock_routes,
                mock_index_routes.mock_routes,
                mock_subtraction_routes.mock_routes,
-               mock_sample_routes.mock_routes]
+               mock_hmm_routes.mock_routes]
