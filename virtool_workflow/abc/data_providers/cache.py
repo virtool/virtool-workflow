3deb1b07c2559300e8e6a223b5d14e1b948ad38e
Blake Smith <blakeinvictoria@gmail.com>
2021-03-12 10:55:10 -0800

    Remove `ABC` from superclasses.

--- virtool_workflow/abc/data_providers/cache.py
+++ virtool_workflow/abc/data_providers/cache.py
@@ -1,4 +1,4 @@
-from abc import ABC, abstractmethod
+from abc import abstractmethod
 from contextlib import AbstractAsyncContextManager
 from pathlib import Path
 
@@ -31,7 +31,7 @@ class AbstractCache(AbstractAsyncContextManager):
         return await self.close()
 
 
-class AbstractCaches(ABC, AbstractAsyncContextManager):
+class AbstractCaches(AbstractAsyncContextManager):
 
     @abstractmethod
     async def get(self, key: str) -> AbstractCache:
