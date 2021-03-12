0d5def92e8e9d1d925e3a0d9b7d127c1eebc7702
Blake Smith <blakeinvictoria@gmail.com>
2021-03-11 16:52:53 -0800

    Add stubs for mocked cache API routes.

---
+++ tests/virtool_workflow/api/mocks/mock_cache_routes.py
@@ -0,0 +1,29 @@
+import tempfile
+from pathlib import Path
+
+from aiohttp.web_routedef import RouteTableDef
+
+cache_path = Path(tempfile.mkdtemp())
+caches = {}
+
+mock_routes = RouteTableDef()
+
+
+@mock_routes.post("/api/cached/{key}")
+def create_cache_placeholder(request):
+    raise NotImplementedError()
+
+
+@mock_routes.post("/api/caches/{key}/files")
+def upload_cache_file(request):
+    raise NotImplementedError()
+
+
+@mock_routes.get("/api/caches/{key}")
+def get_cache(request):
+    raise NotImplementedError()
+
+
+@mock_routes.patch("/api/caches/{key}")
+def finalize(request):
+    raise NotImplementedError()
