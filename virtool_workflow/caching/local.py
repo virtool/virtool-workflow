f535bd951b072acb76f21befe513d4bfc9f3f2f6
Blake Smith <blakeinvictoria@gmail.com>
2021-03-12 12:52:40 -0800

    Implement `LocalCache` and `LocalCaches`.
    Local filesystem caches.

---
+++ virtool_workflow/caching/local.py
@@ -0,0 +1,50 @@
+import shutil
+from pathlib import Path
+
+from virtool_workflow.abc.data_providers.cache import AbstractCache, AbstractCaches
+from virtool_workflow.execution.run_in_executor import FunctionExecutor
+
+
+class LocalCache(AbstractCache):
+    """A cache stored in the local filesystem."""
+
+    def __init__(self, key: str, path: Path, run_in_executor: FunctionExecutor):
+        self.key = key
+        self.path = path
+        self.run_in_executor = run_in_executor
+        self.closed = False
+
+        path.mkdir(parents=True, exist_ok=True)
+
+    async def open(self) -> "AbstractCache":
+        self.closed = False
+        return self
+
+    async def upload(self, path: Path):
+        await self.run_in_executor(shutil.copyfile, path, self.path / path.name)
+
+    async def close(self):
+        self.closed = True
+
+    async def delete(self):
+        await self.run_in_executor(shutil.rmtree, self.path)
+
+
+class LocalCaches(AbstractCaches):
+
+    def __init__(self, path: Path, run_in_executor: FunctionExecutor):
+        self.path = path
+        path.mkdir(parents=True, exist_ok=True)
+
+        self.run_in_executor = run_in_executor
+        self._caches = {}
+
+    async def get(self, key: str) -> AbstractCache:
+        return self._caches[key]
+
+    async def create(self, key: str) -> AbstractCache:
+        self._caches = LocalCache(key, self.path / key, self.run_in_executor)
+        return self._caches[key]
+
+    def __contains__(self, key: str):
+        return key in self._caches
