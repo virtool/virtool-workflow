431ca8bac1571eaa1466442078d4c20b881eb6e2
Blake Smith <blakeinvictoria@gmail.com>
2021-03-12 14:08:26 -0800

    Create reads from cache.

--- tests/virtool_workflow/analysis/test_analysis.py
+++ tests/virtool_workflow/analysis/test_analysis.py
@@ -1,1 +1,32 @@
-async def test_reads_caching():
+from pathlib import Path
+
+from virtool_workflow.abc.caches.analysis_caches import AbstractReadsCache
+from virtool_workflow.caching.local import LocalCache
+from virtool_workflow.execution.run_in_executor import FunctionExecutor, run_in_executor, thread_pool_executor
+
+
+class MockReadsCache(AbstractReadsCache, LocalCache):
+
+    def __init__(self, path: Path, run_in_executor: FunctionExecutor):
+        super(MockReadsCache, self).__init__("test_analysis_cache", path, run_in_executor)
+        self._quality = None
+
+    @property
+    def quality(self):
+        return self._quality
+
+    @quality.setter
+    def quality(self, qc: dict):
+        self._quality = qc
+
+    async def open(self) -> "MockReadsCache":
+        return self
+
+    async def close(self):
+        await AbstractReadsCache.close(self)
+
+
+async def test_reads_caching(tmpdir):
+    read_cache = MockReadsCache(Path(tmpdir), run_in_executor(thread_pool_executor()))
+
+    reads(read_cache)
--- virtool_workflow/analysis/read_prep.py
+++ virtool_workflow/analysis/read_prep.py
@@ -1,5 +1,26 @@
-"""Perform read prep before accessing Virtool reads_path."""
-# pylint: disable=redefined-outer-name
+"""Perform read prep before accessing Virtool reads."""
 import logging
+import shutil
+from pathlib import Path
+
+from virtool_workflow import fixture
+from virtool_workflow.abc.caches.analysis_caches import AbstractReadsCache
+from virtool_workflow.analysis.reads import Reads
+from virtool_workflow.execution.run_in_executor import FunctionExecutor
 
 logger = logging.getLogger(__name__)
+
+
+@fixture
+async def reads(
+        reads_cache: AbstractReadsCache,
+        reads_path: Path,
+        run_in_executor: FunctionExecutor,
+        paired: bool,
+):
+    """A fixture for accessing trimmed reads for the current sample."""
+    if reads_cache.finalized:
+        await run_in_executor(shutil.copytree(reads_cache.path, reads_path))
+        return Reads.from_quality(reads_cache.quality, paired, reads_path)
+    else:
+        raise NotImplementedError()
