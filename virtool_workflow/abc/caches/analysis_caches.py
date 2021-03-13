8c9aba85520011e2ef67f5c943075733095345e4
Blake Smith <blakeinvictoria@gmail.com>
2021-03-12 16:50:18 -0800

    Update analysis fixture test to use `LocalCacheWriter`.

--- virtool_workflow/abc/caches/analysis_caches.py
+++ virtool_workflow/abc/caches/analysis_caches.py
@@ -1,29 +1,8 @@
-from abc import abstractmethod
+from dataclasses import dataclass
 
-from virtool_workflow.abc.caches.cache import AbstractCacheWriter, CacheFileMissing
+from virtool_workflow.abc.caches.cache import Cache
 
 
-class AbstractReadsCache(AbstractCacheWriter):
-
-    @property
-    @abstractmethod
-    def quality(self):
-        """The parsed FastQC quality check output."""
-        ...
-
-    @quality.setter
-    @abstractmethod
-    def quality(self, qc: dict):
-        ...
-
-    async def close(self):
-        if not self.quality:
-            raise ValueError("FastQC quality check output must be provided.")
-
-        cache_files = list(p.name for p in self.path.iterdir())
-
-        if "reads.fq.gz" not in cache_files:
-            if "reads_1.fq.gz" not in cache_files or "reads_2.fq.gz" not in cache_files:
-                raise CacheFileMissing("Trimmed read files have not been uploaded to the read cache.")
-
-        await super(AbstractReadsCache, self).close()
+@dataclass
+class ReadsCache(Cache):
+    quality: dict
--- virtool_workflow/analysis/read_prep.py
+++ virtool_workflow/analysis/read_prep.py
@@ -4,7 +4,7 @@ import shutil
 from pathlib import Path
 
 from virtool_workflow import fixture
-from virtool_workflow.abc.caches.analysis_caches import AbstractReadsCache
+from virtool_workflow.abc.caches.analysis_caches import ReadsCache
 from virtool_workflow.analysis.reads import Reads
 from virtool_workflow.execution.run_in_executor import FunctionExecutor
 
@@ -13,13 +13,13 @@ logger = logging.getLogger(__name__)
 
 @fixture
 async def reads(
-        reads_cache: AbstractReadsCache,
         reads_path: Path,
         run_in_executor: FunctionExecutor,
         paired: bool,
+        reads_cache: ReadsCache = None,
 ):
     """A fixture for accessing trimmed reads for the current sample."""
-    if reads_cache.finalized:
+    if reads_cache:
         await run_in_executor(shutil.copytree, reads_cache.path, reads_path)
         return Reads.from_quality(reads_cache.quality, paired, reads_path)
     else:
