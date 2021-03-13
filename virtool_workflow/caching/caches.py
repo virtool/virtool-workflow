af864d00bbb71522a0e532e3ba832aefdef5ca9c
Blake Smith <blakeinvictoria@gmail.com>
2021-03-12 16:06:47 -0800

    Implement local caching.

--- virtool_workflow/caching/caches.py
+++ virtool_workflow/caching/caches.py
@@ -1,16 +1,18 @@
-from abc import abstractmethod
 from inspect import signature
-from typing import Generic, TypeVar, Type
+from typing import TypeVar
 
 from virtool_workflow.abc.caches.cache import Cache, AbstractCacheWriter, CacheNotFinalized
 
-GenericCache = TypeVar("GenericCache", bound=Type[Cache])
+GenericCache = TypeVar("GenericCache", bound=Cache)
 
 
-class GenericCacheWriter(AbstractCacheWriter, Generic[GenericCache]):
+class GenericCacheWriter(AbstractCacheWriter):
+    cache_class = Cache
 
     def __init__(self):
-        self._attributes = {name: None for name in signature(GenericCache.__init__).parameters}
+        self._attributes = {name: None for name
+                            in signature(self.cache_class.__init__).parameters
+                            if name != "self"}
         self._cache = None
 
     @property
@@ -21,17 +23,29 @@ class GenericCacheWriter(AbstractCacheWriter, Generic[GenericCache]):
             )
         return self._cache
 
-    @abstractmethod
     async def write_cache(self):
         ...
 
     def __setattr__(self, key, value):
         try:
-            self._attributes[key] = value
-        except KeyError:
+            if key in self._attributes:
+                self._attributes[key] = value
+            else:
+                raise KeyError(key)
+        except (KeyError, AttributeError):
             super(GenericCacheWriter, self).__setattr__(key, value)
 
-    async def write(self):
-        self._cache = GenericCache(**self._attributes)
-
+    async def _write(self):
+        self._cache = self.cache_class(**self._attributes)
         await self.write_cache()
+
+    async def open(self):
+        return self
+
+    async def close(self):
+        await self._write()
+        return await super(GenericCacheWriter, self).close()
+
+    def __class_getitem__(cls, item):
+        cls.cache_class = item
+        return cls
