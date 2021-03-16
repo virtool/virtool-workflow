f5b62da140ce647d13a7ab6d815af10e5172a500
Blake Smith <blakeinvictoria@gmail.com>
2021-03-16 16:21:25 -0700

    Ignore leading `_` in fixture names.

--- virtool_workflow/runtime/providers.py
+++ virtool_workflow/runtime/providers.py
@@ -1,4 +1,5 @@
 from virtool_workflow.api import jobs
+from virtool_workflow.api.analysis import AnalysisProvider
 from virtool_workflow.api.scope import api_fixtures
 from virtool_workflow.config import fixtures as config
 from virtool_workflow.fixtures import FixtureGroup
@@ -7,5 +8,10 @@ providers = FixtureGroup(config.job_id, jobs.acquire_job, **api_fixtures)
 
 
 @providers.fixture
-async def job(job_id, acquire_job):
+async def _job(job_id, acquire_job):
     return acquire_job(job_id)
+
+
+@providers.fixture
+async def analysis_provider(job, http):
+    return AnalysisProvider(job.args["analysis_id"], http)
