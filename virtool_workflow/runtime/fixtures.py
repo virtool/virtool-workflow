c1c70fa19852f0ca5207c03d20f722ee774e9bad
Blake Smith <blakeinvictoria@gmail.com>
2021-03-16 16:49:09 -0700

    Add analysis fixtues to `analysis` fixture group.

--- virtool_workflow/runtime/fixtures.py
+++ virtool_workflow/runtime/fixtures.py
@@ -1,8 +1,14 @@
+from virtool_workflow.analysis import fixtures as analysis_fixtures
 from virtool_workflow.fixtures import FixtureGroup
+from virtool_workflow.runtime.providers import providers
 
-runtime = FixtureGroup()
+runtime = FixtureGroup(**providers)
 """A :class:`FixtureGroup` containing all fixtures available to the runtime."""
-workflow = FixtureGroup()
+
+workflow = FixtureGroup(providers["job"])
 """A :class:`FixtureGroup` containing all fixtures available within workflows."""
-analysis = FixtureGroup()
+
+analysis = FixtureGroup(**providers,
+                        **{k: getattr(analysis_fixtures, k)
+                           for k in analysis_fixtures.__all__})
 """A :class:`FixtureGroup` containing all fixtures excusive to analysis workflows."""
