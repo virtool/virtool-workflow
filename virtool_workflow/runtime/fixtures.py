367c55c0000e3e89d1adc43d60e42cfc7d89e83a
Blake Smith <blakeinvictoria@gmail.com>
2021-03-16 16:10:15 -0700

    Add docstrings.

--- virtool_workflow/runtime/fixtures.py
+++ virtool_workflow/runtime/fixtures.py
@@ -1,5 +1,8 @@
 from virtool_workflow.fixtures import FixtureGroup
 
 runtime = FixtureGroup()
+"""A :class:`FixtureGroup` containing all fixtures available to the runtime."""
 workflow = FixtureGroup()
+"""A :class:`FixtureGroup` containing all fixtures available within workflows."""
 analysis = FixtureGroup()
+"""A :class:`FixtureGroup` containing all fixtures excusive to analysis workflows."""
