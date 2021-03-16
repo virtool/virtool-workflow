4713e0d41c12988939eb780ac6bc0eb6a9834f4b
Blake Smith <blakeinvictoria@gmail.com>
2021-03-15 19:53:20 -0700

    Perform trimming and fastqc if no cache available.

--- virtool_workflow/analysis/read_caching/reads_cache.py
+++ virtool_workflow/analysis/read_caching/reads_cache.py
@@ -2,12 +2,30 @@ from contextlib import suppress
 
 import virtool_workflow
 from virtool_workflow.abc.caches.analysis_caches import ReadsCache
+from virtool_workflow.analysis import trim_parameters
+from virtool_workflow.analysis.read_caching.fastqc import FastQC
+from virtool_workflow.analysis.read_caching.trimming import Trimming
 from virtool_workflow.caching.caches import GenericCaches
 from virtool_workflow.data_model import Sample
 
 
 @virtool_workflow.fixture
-async def reads_cache(sample: Sample, caches: GenericCaches[ReadsCache], trimming, fastqc) -> ReadsCache:
+async def trimming(sample: Sample, number_of_processes: int, run_subprocess) -> Trimming:
+    parameters = trim_parameters.trimming_parameters(
+        sample.library_type,
+        trim_parameters.trimming_min_length(sample.library_type, sample.quality["length"])
+    )
+
+    return Trimming(parameters, number_of_processes, run_subprocess)
+
+
+@virtool_workflow.fixture
+async def fastqc(work_path, run_subprocess) -> FastQC:
+    return FastQC(work_path, run_subprocess)
+
+
+@virtool_workflow.fixture
+async def reads_cache(sample: Sample, caches: GenericCaches[ReadsCache], trimming: Trimming, fastqc) -> ReadsCache:
     cache_key = f'{sample.id}-reads-{hash(trimming)}'
 
     with suppress(KeyError):
@@ -15,10 +33,9 @@ async def reads_cache(sample: Sample, caches: GenericCaches[ReadsCache], trimmin
 
     async with caches.create(cache_key) as cache:
         await trimming.run_trimming(output_path=cache.path)
-       
+        cache.quality = await fastqc.run(cache.path)
+
         for path in cache.path.iterdir():
             await cache.upload(path)
-
-        cache.quality = await fastqc.run(cache.path)
 
     return await caches.get(cache_key)
