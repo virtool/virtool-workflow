adf87e11406f8f4629049f61270a69633e618a3e
Blake Smith <blakeinvictoria@gmail.com>
2021-03-16 14:06:46 -0700

    Implement upload and download routes.

--- virtool_workflow/abc/data_providers/samples.py
+++ virtool_workflow/abc/data_providers/samples.py
@@ -2,6 +2,7 @@ from abc import ABC, abstractmethod
 from pathlib import Path
 from typing import Dict, Any
 
+from virtool_workflow.analysis.utils import ReadPaths
 from virtool_workflow.data_model import Sample
 from virtool_workflow.data_model.files import VirtoolFileFormat
 
@@ -28,9 +29,24 @@ class AbstractSampleProvider(ABC):
         ...
 
     @abstractmethod
-    async def download(self):
-        """Delete files associated with the current sample."""
+    async def download_reads(self, target_path: Path, paired: bool = None) -> ReadPaths:
+        """
+        Download reads for the current sample.
+
+        :param target_path: The path where the file(s) will be downloaded.
+        :param paired: Indicates that the sample is paired. If not provided then the sample
+            will be fetched again via `.get()`.
+        """
         ...
+
+    @abstractmethod
+    async def download_artifact(self, filename: str, target_path: Path):
+        """
+        Download an artifact associated with the current sample.
+
+        :param: The file name of the artifact.
+        :param: The path where the file should be stored.
+        """
 
     def __await__(self):
         return self.get().__await__()
