096b249c49f1bcfaeb9eb8bea285f6a61c377f23
Blake Smith <blakeinvictoria@gmail.com>
2021-03-16 17:08:23 -0700

    Use `workflow` fixture group in `WorkflowEnvironment`.

--- virtool_workflow/environment.py
+++ virtool_workflow/environment.py
@@ -3,8 +3,8 @@ from typing import Dict, Any
 from virtool_workflow import hooks
 from virtool_workflow.abc import AbstractWorkflowEnvironment
 from virtool_workflow.execution.workflow_execution import WorkflowExecution
-from virtool_workflow.fixtures import workflow_fixtures
 from virtool_workflow.fixtures.scope import FixtureScope
+from virtool_workflow.runtime import fixtures
 from virtool_workflow.workflow import Workflow
 
 
@@ -12,7 +12,7 @@ class WorkflowEnvironment(AbstractWorkflowEnvironment, FixtureScope):
 
     def __init__(self, *providers, **instances):
         super(WorkflowEnvironment, self).__init__(
-            workflow_fixtures,
+            fixtures.workflow,
             *providers,
             **instances)
 
