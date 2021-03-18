a1a9ee39d893229c9010471375475a48debd008c
Blake Smith <blakeinvictoria@gmail.com>
2021-03-18 10:28:29 -0700

    Move `fastqc` to `read_prep` package.

--- virtool_workflow/analysis/read_prep/fastqc.py
+++ virtool_workflow/analysis/read_prep/fastqc.py
@@ -2,24 +2,22 @@ from virtool_workflow.analysis.fastqc import parse_fastqc
 from virtool_workflow.analysis.utils import ReadPaths
 
 
-class FastQC:
-    def __init__(self, work_path, run_subprocess):
-        self.work_path = work_path
-        self.path = work_path / "fastqc"
-        self.path.mkdir()
+def fastqc(work_path, run_subprocess):
+    fastqc_path = work_path / "fastqc"
+    fastqc_path.mkdir()
 
-        self.run_subprocess = run_subprocess
-
-    async def run(self, input_paths: ReadPaths):
+    async def run_fastqc(self, input_paths: ReadPaths):
         """Run fastqc on the input path and return the parsed result."""
         command = [
             "fastqc",
             "-f", "fastq",
-            "-o", str(self.path),
+            "-o", str(fastqc_path),
             "--extract",
             *[str(path) for path in input_paths]
         ]
 
-        await self.run_subprocess(command)
+        await run_subprocess(command)
 
         return parse_fastqc(self.path, self.path)
+
+    return run_fastqc
