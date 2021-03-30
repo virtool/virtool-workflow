907ce9b85cc756ad3b6f46aa59cc253f7969b198
Blake Smith <blakeinvictoria@gmail.com>
2021-03-29 17:02:54 -0700

    Use trap to ensure temp files get deleted.

--- integration_test_workflow/build.sh
+++ integration_test_workflow/build.sh
@@ -1,16 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
 temp=$(mktemp -d build-vt-jobs-ap1-XXX)
-
 cwd=$(pwd)
+
 dockerfile=$(realpath api-server-Dockerfile)
 
+# Ensure temporary directory is removed and return to previous directory
+cmd="cd $cwd && rm -rf $temp"
+trap "$cmd" SIGINT
+trap "$cmd" EXIT
+
 cd $temp
+
 # Clone virtool/virtool before building `virtool/jobs-api` docker container
-git clone --single-branch --branch release/5.0.0 https://github.com/virtool/virtool.git
-cd virtool
-
+git clone --single-branch --branch ${1:-release/5.0.0} ${2:-https://github.com/virtool/virtool.git} && \
+cd virtool && \
 docker build -t virtool/jobs-api -f $dockerfile .
-
-cd $cwd
-rm -rf $tmp_dir
