e944bc766d7f381ad34cd1c9f49e6f72eacca2d5
Blake Smith <blakeinvictoria@gmail.com>
2021-04-01 12:41:16 -0700

    Support using `ModuleType` instead of workflow instance.

--- virtool_workflow/workflow_feature/merge_workflows.py
+++ virtool_workflow/workflow_feature/merge_workflows.py
@@ -1,11 +1,13 @@
 from virtool_workflow.workflow import Workflow
 from virtool_workflow.features import WorkflowFeature
+from virtool_workflow.decorator_api import collect
+from typing import Union
 
 
 class MergeWorkflows(WorkflowFeature):
     """Merge steps from other workflows into the current workflow."""
-    def __init__(self, *workflows: Workflow):
-        self.workflows = workflows
+    def __init__(self, *workflows: Union[Workflow, ModuleType]):
+        self.workflows = [workflow if isinstance(workflow, Workflow) else collect(workflow) for workflow in workflows]
 
     def __modify_workflow__(workflow: Workflow):
         return workflow.merge(*self.workflows)
