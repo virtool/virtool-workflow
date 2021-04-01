b1167b4626eee66b173f0732188fe6aae8d3b865
Blake Smith <blakeinvictoria@gmail.com>
2021-03-31 18:37:41 -0700

    Get tests passing.

--- tests/virtool_workflow/analysis/test_skewer.py
+++ tests/virtool_workflow/analysis/test_skewer.py
@@ -2,7 +2,10 @@ import gzip
 import shutil
 from pathlib import Path
 
-from tests.virtool_workflow.api.mocks.mock_sample_routes import TEST_SAMPLE_ID, TEST_SAMPLE
+from tests.virtool_workflow.api.mocks.mock_sample_routes import (
+    TEST_SAMPLE_ID,
+    TEST_SAMPLE,
+)
 from virtool_workflow import features, Workflow
 from virtool_workflow.abc.caches.analysis_caches import ReadsCache
 from virtool_workflow.analysis.features.trimming import Trimming
@@ -12,28 +15,33 @@ from virtool_workflow.data_model import Job
 from virtool_workflow.runtime.providers import sample_provider
 
 
-async def test_skewer(http, jobs_api_url,
-                      tmpdir, run_subprocess, run_in_executor,
-                      analysis_files, file_regression):
+async def test_skewer(
+    http,
+    jobs_api_url,
+    tmpdir,
+    run_subprocess,
+    run_in_executor,
+    analysis_files,
+    file_regression,
+):
     tmpdir = Path(tmpdir)
 
-    job = Job(
-        "test_job",
-        {
-            "sample_id": TEST_SAMPLE_ID
-        },
-    )
+    job = Job("test_job", {"sample_id": TEST_SAMPLE_ID},)
     sample = await sample_provider(job, http, jobs_api_url).get()
 
     run_skewer = skewer(
         min_length=trimming_min_length(sample.library_type, sample.max_length),
-        quiet=True
+        quiet=True,
     )
 
     TEST_READ_1 = analysis_files / "paired_small_1.fq.gz"
     TEST_READ_2 = analysis_files / "paired_small_2.fq.gz"
-    read_1 = await run_in_executor(shutil.copyfile, TEST_READ_1, tmpdir / TEST_READ_1.name)
-    read_2 = await run_in_executor(shutil.copyfile, TEST_READ_2, tmpdir / TEST_READ_2.name)
+    read_1 = await run_in_executor(
+        shutil.copyfile, TEST_READ_1, tmpdir / TEST_READ_1.name
+    )
+    read_2 = await run_in_executor(
+        shutil.copyfile, TEST_READ_2, tmpdir / TEST_READ_2.name
+    )
 
     reads = (read_1, read_2)
 
@@ -48,16 +56,16 @@ async def test_skewer(http, jobs_api_url,
             file_regression.check(left.read(), basename="left", binary=True)
 
 
-async def test_trimming_feature(runtime, tmpdir,
-                                http,
-                                run_in_executor,
-                                data_regression,
-                                file_regression,
-                                analysis_files):
+async def test_trimming_feature(
+    runtime,
+    tmpdir,
+    http,
+    run_in_executor,
+    data_regression,
+    file_regression,
+    analysis_files,
+):
     runtime["http"] = http
-||||||| parent of f5ca739... Reduce CI Test Runtime (#133)
-async def test_trimming_feature(runtime, tmpdir, http_no_decompress, run_in_executor, data_regression):
-    runtime["http"] = http_no_decompress
     TEST_SAMPLE["paired"] = True
     runtime["sample_caches"] = LocalCaches[ReadsCache](Path(tmpdir), run_in_executor)
     job = await runtime.get_or_instantiate("job")
@@ -71,9 +79,17 @@ async def test_trimming_feature(runtime, tmpdir, http_no_decompress, run_in_exec
     sample_provider = await runtime.get_or_instantiate("sample_provider")
 
     async def copy_read_files(target, paired):
-        await run_in_executor(shutil.copyfile, analysis_files / "paired_small_1.fq.gz", target / "reads_1.fq.gz")
+        await run_in_executor(
+            shutil.copyfile,
+            analysis_files / "paired_small_1.fq.gz",
+            target / "reads_1.fq.gz",
+        )
         if paired:
-            await run_in_executor(shutil.copyfile, analysis_files / "paired_small_2.fq.gz", target / "reads_2.fq.gz")
+            await run_in_executor(
+                shutil.copyfile,
+                analysis_files / "paired_small_2.fq.gz",
+                target / "reads_2.fq.gz",
+            )
 
     sample_provider.download_reads = copy_read_files
 
