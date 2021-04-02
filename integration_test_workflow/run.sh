4e00fb44e0aec5ed128cc81c91bb476f16ae49f6
Blake Smith <blakeinvictoria@gmail.com>
2021-04-01 19:02:52 -0700

    Select branch and repo in build script.
    - Adds --virtool-repo, --virtool-workflow-repo, and --virtool-branch
    options
    - Support use of forks
    - Adds --help to build script

--- integration_test_workflow/run.sh
+++ integration_test_workflow/run.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-indent() { sed 's/^/  /'; }
+format='\e[1;34m%-6s\e[m'
 
-printf '\e[1;34m%-6s\e[m' $'Running build script...\n'
-./build.sh | indent
-printf '\e[1;34m%-6s\e[m' $'Starting docker-compose...\n'
-docker-compose up --exit-code-from=integration_test_workflow | indent
+_print() {
+    echo -e "\n"
+    printf $format "$1"
+    echo -e "\n"
+}
+
+_print 'Running build script...'
+./build.sh $@
+_print 'Starting docker-compose...'
+docker-compose up --exit-code-from=integration_test_workflow
+_print "DONE"
