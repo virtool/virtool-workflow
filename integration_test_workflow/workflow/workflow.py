54321cb152fabb853902d1a8c2d54f52c2e4a413
Blake Smith <blakeinvictoria@gmail.com>
2021-04-01 17:03:06 -0700

    Test for presence of built-in fixtures.

--- integration_test_workflow/workflow/workflow.py
+++ integration_test_workflow/workflow/workflow.py
@@ -1,12 +1,10 @@
-from integration_test_workflows import (
-    test_hmms,
-    test_indexes,
-    test_sample,
-    test_subtractions,
-)
+from integration_test_workflows import (test_hmms, test_indexes, test_sample,
+                                        test_subtractions)
 from virtool_workflow import features
 from virtool_workflow.analysis.features.trimming import Trimming
 from virtool_workflow.decorator_api import collect, step
+from virtool_workflow.execution.run_in_executor import FunctionExecutor
+from virtool_workflow.execution.run_subprocess import RunSubprocess
 from virtool_workflow.workflow import Workflow
 from virtool_workflow.workflow_feature.merge_workflows import MergeWorkflows
 
@@ -25,3 +23,10 @@ features.install(
 def test_results_available(results):
     assert results is not None
     assert isinstance(results, dict)
+
+
+@step
+def test_builtin_fixtures_available(workflow, run_in_executor, run_subprocess)
+    assert isinstance(workflow, Workflow)
+    assert isinstance(run_in_executor, FunctionExecutor)
+    assert isinstance(run_subprocess,  RunSubprocess)
