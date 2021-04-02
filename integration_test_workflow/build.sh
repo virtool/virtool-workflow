a284725d41432ee7c80528ee64e0c953fe723b00
Blake Smith <blakeinvictoria@gmail.com>
2021-04-01 18:07:30 -0700

    Get the latest `virtool_workflow` from github during build.

--- integration_test_workflow/build.sh
+++ integration_test_workflow/build.sh
@@ -5,13 +5,20 @@ cwd=$(pwd)
 
 dockerfile=$(realpath api-server-Dockerfile)
 test_dockerfile=$(realpath workflow/Dockerfile)
+workflow_latest_dockerfile=$(realpath latest-workflow-Dockerfile)
+
+# Build `virtool/workflow` with the latest changes
+docker build -t virtool/workflow -f $workflow_latest_dockerfile .
+
+
+# Build integration test workflow container
+docker build -t virtool/integration_test_workflow -f $test_dockerfile workflow
 
 # Ensure temporary directory is removed and return to previous directory
 cmd="cd $cwd && rm -rf $temp"
 trap "$cmd" SIGINT
 trap "$cmd" EXIT
 
-docker build -t virtool/integration_test_workflow -f $test_dockerfile .
 
 cd $temp
 
