2fd4d8ec74209b328c7229366b9bfe0153cfd978
Blake Smith <blakeinvictoria@gmail.com>
2021-04-01 19:02:52 -0700

    Select branch and repo in build script.
    - Adds --virtool-repo, --virtool-workflow-repo, and --virtool-branch
    options
    - Support use of forks
    - Adds --help to build script

--- integration_test_workflow/build.sh
+++ integration_test_workflow/build.sh
@@ -1,18 +1,76 @@
 #!/bin/bash
 
-temp=$(mktemp -d build-vt-jobs-ap1-XXX)
+format='\e[1;34m%-6s\e[m'
+
+_print_one_line(){
+    printf $format "$1"
+    echo -e "\n"
+}
+
+_print() {
+    echo -e "\n"
+    _print_one_line "$1"
+}
+
+
+_help() {
+    echo "Build the docker images for the 'virtool_workflow' integration tests."
+    echo
+    echo "Syntax: ./build.sh [--virtool-repo|--virtool-workflow-repo]"
+    echo "Options:"
+    echo "--virtool-repo             The URL of the virtool github repo (to use your fork)."
+    echo "--virtool-branch           The name of the branch to pull from the virtool repo." 
+    echo "--virtool-workflow-repo    The URL of the virtool-workflow github repo (to use your fork)."
+    echo
+}
+
+
+# Parse options
+while [[ $# -gt 0 ]] 
+do
+    key="$1"
+    case $key in
+        --virtool-workflow-repo)
+            VIRTOOL_WORKFLOW_REPO="$2"
+            shift
+            shift 
+        ;;
+        --virtool-repo)
+            VIRTOOL_REPO="$2"
+            shift
+            shift 
+        ;;
+        --virtool-branch)
+            VIRTOOL_BRANCH="$2"
+            shift 
+            shift
+        ;;
+        --help)
+            _help
+            exit
+        ;;
+        
+    esac
+done
+
+virtool_workflow_repo=${VIRTOOL_WORKFLOW_REPO:-"https://github.com/virtool/virtool-workflow"}
+virtool_repo=${VIRTOOL_REPO:-"https://github.com/virtool/virtool"}
+virtool_branch=${VIRTOOL_BRANCH:-"release/5.0.0"}
+
 cwd=$(pwd)
 
 dockerfile=$(realpath api-server-Dockerfile)
 test_dockerfile=$(realpath workflow/Dockerfile)
 workflow_latest_dockerfile=$(realpath latest-workflow-Dockerfile)
 
-# Build `virtool/workflow` with the latest changes
-docker build -t virtool/workflow -f $workflow_latest_dockerfile .
+_print "Building 'virtool/workflow' image with the latest version of 'virtool_workflow'..."
+docker build -t virtool/workflow -f $workflow_latest_dockerfile --build-arg "VIRTOOL_WORKFLOW_REPO=$virtool_workflow_repo" .
 
-
-# Build integration test workflow container
+_print "Building 'virtool/integration_test_workflow'..."
 docker build -t virtool/integration_test_workflow -f $test_dockerfile workflow
+
+temp=$(mktemp -d build-vt-jobs-ap1-XXX)
+_print_one_line "Created temporary directory... $temp"
 
 # Ensure temporary directory is removed and return to previous directory
 cmd="cd $cwd && rm -rf $temp"
@@ -22,8 +80,10 @@ trap "$cmd" EXIT
 
 cd $temp
 
-# Clone virtool/virtool before building `virtool/jobs-api` docker container
-git clone --single-branch --branch ${1:-release/5.0.0} ${2:-https://github.com/virtool/virtool.git} && \
+_print "Building 'virtool/job-api' with the latest changes..."
+git clone --single-branch --branch $virtool_branch $virtool_repo && \
 cd virtool && \
 
 docker build -t virtool/jobs-api -f $dockerfile .
+
+_print DONE
