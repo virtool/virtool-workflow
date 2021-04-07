54bec2a1503c92d6d9ea08ad39f15d48228a72b7
Blake Smith <blakeinvictoria@gmail.com>
2021-04-07 12:10:01 -0700

    Log updates in integration test workflow.

--- integration_test_workflow/workflow/workflow.py
+++ integration_test_workflow/workflow/workflow.py
@@ -1,6 +1,11 @@
-from integration_test_workflows import (test_hmms, test_indexes, test_sample,
-                                        test_subtractions)
-from virtool_workflow import features, hooks
+import logging
+from integration_test_workflows import (
+    test_hmms,
+    test_indexes,
+    test_sample,
+    test_subtractions,
+)
+from virtool_workflow import features, hooks, fixture
 from virtool_workflow.analysis.features.trimming import Trimming
 from virtool_workflow.decorator_api import collect, step
 from virtool_workflow.execution.run_in_executor import FunctionExecutor
@@ -15,21 +20,29 @@ features.install(
 )
 
 
+@fixture
+def logger():
+    return logging.getLogger(__name__)
+
+
+@hooks.on_update
+def log_updates(update, logger):
+    logger.info(f"Sent update: {update}")
+
+
 @step
 def test_results_available(results):
     assert results is not None
     assert isinstance(results, dict)
+    return "Results fixture available"
 
 
 @step
-def test_builtin_fixtures_available(
-        workflow, 
-        run_in_executor, 
-        run_subprocess
-):
+def test_builtin_fixtures_available(workflow, run_in_executor, run_subprocess):
     assert isinstance(workflow, Workflow)
     assert isinstance(run_in_executor, FunctionExecutor)
     assert isinstance(run_subprocess, RunSubprocess)
+    return "Builtin fixtures available"
 
 
 @step
@@ -45,3 +58,5 @@ async def test_execution_fixture(execution):
     await execution.send_update("UPDATE")
 
     assert "UPDATE" in updates
+
+    return "`execution.send_update()` working"
