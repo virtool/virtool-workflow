6e4ba56c875fca68ce0d5cb1c0beb6b3aab44a5e
Blake Smith <blakeinvictoria@gmail.com>
2021-04-09 13:37:17 -0700

    Port build script to python.

--- virtool-integration/virtool_integration/cli.py
+++ virtool-integration/virtool_integration/cli.py
@@ -1,4 +1,13 @@
+import tempfile
+from pathlib import Path
+
 import click
+
+from .utils import DONE, bcolors, clone, color, docker_build
+
+JOBS_API_DOCKERFILE = Path(__file__).parent / "api-server-Dockerfile"
+WORKFLOW_DOCKER_FILE = Path(__file__).parent / "latest-workflow-local-Dockerfile"
+INTEGRATION_WORKFLOW_DOCKER_FILE = Path(__file__).parent / "workflow/Dockerfile"
 
 
 @click.group()
@@ -6,6 +15,113 @@ def cli():
     ...
 
 
-@cli.command()
+@cli.group()
 def build():
+    """Build the required docker images with the latest version of the code."""
     ...
+
+
+@click.option(
+    "--path",
+    default="..",
+    type=click.Path(),
+    help="The path to a local clone of the virtool-workflow git repository.",
+)
+@click.option(
+    "--remote", default=None, help="The virtool-workflow git repository to pull from."
+)
+@build.command()
+def workflow(path, remote):
+    """Build the `virtool/workflow` image."""
+    tag = "virtool/workflow"
+    if remote is not None:
+        tempd = tempfile.mkdtemp()
+        print(color(bcolors.OKBLUE, f"Created temporary directory {tempd}..."))
+        print(color(bcolors.OKBLUE, f"Cloning {remote}...\n"))
+        clone(remote, cwd=tempd)
+        context = Path(tempd) / "virtool-workflow"
+    else:
+        context = path
+
+    print(color(bcolors.OKBLUE, f"\nBuilding `{color(bcolors.OKGREEN, tag)}`...\n"))
+    docker_build(
+        dockerfile=WORKFLOW_DOCKER_FILE, context=context, tag=tag,
+    )
+
+
+@build.command()
+def integration():
+    """Build the `virtool/integration_test_workflow` image."""
+    tag = "virtool/integration_test_workflow"
+    print(color(bcolors.OKBLUE, f"\nBuilding `{color(bcolors.OKGREEN, tag)}`...\n"))
+    docker_build(
+        dockerfile=INTEGRATION_WORKFLOW_DOCKER_FILE,
+        context=Path(__file__).parent / "workflow",
+        tag=tag,
+    )
+    print(DONE)
+
+
+@click.option(
+    "--remote",
+    default="https://github.com/virtool/virtool",
+    help="The virtool git repository to pull from.",
+)
+@click.option(
+    "--path",
+    default=None,
+    help="The path to a local clone of the virtool git repository.",
+)
+@build.command()
+def jobs_api(path, remote):
+    """Build the `virtool/jobs-api` image."""
+    tag = "virtool/jobs-api"
+    if path is None:
+        tempd = tempfile.mkdtemp()
+        print(color(bcolors.OKBLUE, f"Created temporary directory {tempd}..."))
+        print(color(bcolors.OKBLUE, f"Cloning {remote}...\n"))
+        clone(remote, cwd=tempd)
+
+        context = Path(tempd) / "virtool"
+    else:
+        context = path
+
+    print(color(bcolors.OKBLUE, f"\nBuilding `{color(bcolors.OKGREEN, tag)}`...\n"))
+
+    docker_build(
+        dockerfile=JOBS_API_DOCKERFILE, context=context, tag=tag,
+    )
+
+    print(DONE)
+
+
+@click.option(
+    "--virtool-remote",
+    default="https://github.com/virtool/virtool",
+    help="The virtool git repository to pull from.",
+)
+@click.option(
+    "--virtool-path",
+    default=None,
+    help="The path to a local clone of the virtool git repository.",
+)
+@click.option(
+    "--virtool-workflow-path",
+    default="..",
+    type=click.Path(),
+    help="The path to a local clone of the virtool-workflow git repository.",
+)
+@click.option(
+    "--virtool-workflow-remote",
+    default=None,
+    help="The virtool-workflow git repository to pull from.",
+)
+@build.command()
+@click.pass_context
+def all(
+    ctx, virtool_remote, virtool_path, virtool_workflow_remote, virtool_workflow_path
+):
+    """Build all of the required images."""
+    ctx.invoke(workflow, remote=virtool_workflow_remote, path=virtool_workflow_path)
+    ctx.invoke(integration)
+    ctx.invoke(jobs_api, remote=virtool_remote, path=virtool_path)
---
+++ virtool-integration/virtool_integration/utils.py
@@ -0,0 +1,44 @@
+import os
+from shutil import which
+from subprocess import call
+
+
+class bcolors:
+    HEADER = "\033[95m"
+    OKBLUE = "\033[94m"
+    OKCYAN = "\033[96m"
+    OKGREEN = "\033[92m"
+    WARNING = "\033[93m"
+    FAIL = "\033[91m"
+    ENDC = "\033[0m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
+
+
+def color(color, text):
+    return f"{color}{text}{bcolors.ENDC}"
+
+
+DONE = color(bcolors.OKGREEN, color(bcolors.BOLD, "\nDONE\n"))
+
+
+def clone(repo: str, branch: str = None, cwd: os.PathLike = None):
+    if not branch:
+        if "@" in repo:
+            repo, branch = repo.split("@")
+        else:
+            branch = "master"
+
+    if which("git") is None:
+        raise RuntimeError("Git is not installed.")
+
+    status = call(
+        f"git clone --single-branch --branch {branch} {repo}", shell=True, cwd=cwd
+    )
+
+    if status != 0:
+        raise RuntimeError(f"Unable to clone {repo}")
+
+
+def docker_build(dockerfile: os.PathLike, context: os.PathLike, tag: str, *args):
+    call(f"docker build -t {tag} -f {dockerfile} {' '.join(args)} .", shell=True, cwd=context)
