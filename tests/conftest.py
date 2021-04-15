b200ef4bc77bbf936b6cc50d870a72c61c7c728e
Blake Smith <blakeinvictoria@gmail.com>
2021-04-14 17:04:27 -0700

    Use `pytest-docker` to make services available.

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,14 +5,15 @@ import pytest
 pytest_plugins = [
     "tests.virtool_workflow.fixtures.workflow",
     "tests.virtool_workflow.fixtures.execution",
-    "tests.standalone_runner.fixtures",
-    "tests.virtool_workflow.api.fixtures",
-    "virtool_workflow.testing",
+    "tests.standalone_runner.fixtures", "tests.virtool_workflow.api.fixtures",
+    "virtool_workflow.testing"
 ]
 
 
 def pytest_addoption(parser):
-    parser.addoption("--db-connection-string", action="store", default="mongodb://localhost:27017")
+    parser.addoption("--db-connection-string",
+                     action="store",
+                     default="mongodb://localhost:27017")
 
 
 @pytest.fixture
@@ -37,3 +38,21 @@ def analysis_files() -> Path:
 @pytest.fixture
 def work_path(tmpdir):
     return Path(tmpdir)
+
+
+@pytest.fixture(scope="session")
+def docker_compose_files(pytestconfig):
+    return [str(Path(__file__).parent / "docker-compose.pytest.yml")]
+
+
+@pytest.fixture(scope="session", autouse=True)
+def redis_url(docker_services):
+    docker_services.start('redis')
+    return f"redis://{docker_services.docker_ip}:6379"
+
+
+@pytest.fixture(scope="session")
+def real_jobs_api_url(docker_services):
+    docker_services.start('job-api')
+    port = docker_services.wait_for_service("job-api", 9990)
+    return f"http://{docker_services.docker_ip}:{port}/api"
