de8c1e4d2444bdac1016d8bf792c15fa3bde6ced
Blake Smith <blakeinvictoria@gmail.com>
2021-04-15 12:18:15 -0700

    Raise an exception if skewer is not installed.

--- virtool_workflow/analysis/read_prep/skewer.py
+++ virtool_workflow/analysis/read_prep/skewer.py
@@ -35,16 +35,18 @@ def skewer(
         other_options: Iterable[str] = ("-n", "-z"),
 ):
     """Create a coroutine function that will run skewer with the given parameters."""
+    if shutil.which("skewer") is None:
+        raise RuntimeError("skewer is not installed.")
+
     command = [
-        "skewer",
-        "-r", str(max_error_rate),
-        "-d", str(max_indel_rate),
-        "-m", str(mode),
-        "-l", str(min_length),
-        "-q", str(end_quality),
-        "-Q", str(mean_quality),
-        "-t", str(number_of_processes),
-        *other_options
+        "skewer", "-r",
+        str(max_error_rate), "-d",
+        str(max_indel_rate), "-m",
+        str(mode), "-l",
+        str(min_length), "-q",
+        str(end_quality), "-Q",
+        str(mean_quality), "-t",
+        str(number_of_processes), *other_options
     ]
 
     if quiet:
@@ -83,18 +85,16 @@ def rename_trimming_results(path: Path):
         return (shutil.move(
             path / "reads-trimmed.fastq.gz",
             path / "reads_1.fq.gz",
-        ),)
+        ), )
     except FileNotFoundError:
-        return (
-            shutil.move(
-                path / "reads-trimmed-pair1.fastq.gz",
-                path / "reads_1.fq.gz",
-            ),
-            shutil.move(
-                path / "reads-trimmed-pair2.fastq.gz",
-                path / "reads_2.fq.gz",
-            )
-        )
+        return (shutil.move(
+            path / "reads-trimmed-pair1.fastq.gz",
+            path / "reads_1.fq.gz",
+        ),
+                shutil.move(
+                    path / "reads-trimmed-pair2.fastq.gz",
+                    path / "reads_2.fq.gz",
+                ))
 
 
 def trimming_min_length(library_type: LibraryType, sample_read_length: int):
