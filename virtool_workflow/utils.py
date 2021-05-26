e259ee91c717f2cc32971fed421f4cfb3af88de9
Blake Smith <blakeinvictoria@gmail.com>
2021-05-26 13:47:37 -0700

    Custom `partial`, avoids long log messages.

--- virtool_workflow/utils.py
+++ virtool_workflow/utils.py
@@ -3,6 +3,22 @@ from inspect import iscoroutinefunction
 from functools import wraps, partial
 
 
+class WrappedPartial:
+    def __init__(self, func, *args, **kwargs):
+        self.partial = partial(func, *args, **kwargs)
+        self.__wrapped__ = func
+        self.__name__ = func.__name__
+
+    def __call__(self, *args, **kwargs):
+        return self.partial(*args, **kwargs)
+
+    def __str__(self):
+        return str(self.__wrapped__)
+
+    def __repr__(self):
+        return str(self.__wrapped__)
+
+
 def coerce_to_coroutine_function(func: Callable):
     """Wrap a non-async function in an async function."""
     if iscoroutinefunction(func):
@@ -31,4 +47,4 @@ def coerce_coroutine_function_to_accept_any_parameters(func: Callable[[], Corout
 
 
 def wrapped_partial(func, *args, **kwargs):
-    return wraps(func)(partial(func, *args, **kwargs))
+    return WrappedPartial(func, *args, **kwargs)
