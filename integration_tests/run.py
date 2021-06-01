fb2095a668883ad67e1a071eb59b12564e9dc84a
Blake Smith <blakeinvictoria@gmail.com>
2021-06-01 13:15:25 -0700

    Add script to run all integration tests.

---
+++ integration_tests/run.py
@@ -0,0 +1,31 @@
+import click
+from pathlib import Path
+from subprocess import call
+
+root_dir = Path(__file__).parent
+
+
+@click.command()
+def run_integration():
+    test_cases = (dir_ for dir_ in root_dir.iterdir() if dir_.is_dir())
+    for test_case_dir in test_cases:
+        return_code = call(
+            [
+                "docker-compose",
+                "up",
+                "--build",
+                "--abort-on-container-exit",
+                "--force-recreate",
+                "--exit-code-from",
+                "workflow"
+            ],
+            cwd=test_case_dir,
+        )
+
+        if return_code != 0:
+            raise RuntimeError(
+                f"{test_case_dir} exited with error code {return_code}")
+
+
+if __name__ == "__main__":
+    run_integration()
