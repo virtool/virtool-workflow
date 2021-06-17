eda579205d3db3fe846a1f69bbb2df6c861b0139
Blake Smith <blakeinvictoria@gmail.com>
2021-06-17 13:42:47 -0700

    Remove tests which are no longer relevant.

--- tests/virtool_workflow/config/test_config.py
+++ tests/virtool_workflow/config/test_config.py
@@ -10,22 +10,6 @@ def click_runner():
     return click.testing.CliRunner()
 
 
-def test_fixture_group_adds_command_line_option():
-    grp = ConfigFixtureGroup()
-
-    @grp.fixture(type_=int, default=0)
-    def some_config_option():
-        """Some config option"""
-        ...
-
-    fixture = grp[some_config_option.__name__]
-
-    assert fixture.name == fixture.__name__ == "some_config_option"
-    assert fixture.type == int
-    assert fixture.help == "Some config option"
-    assert fixture.env == "VT_SOME_CONFIG_OPTION"
-
-
 def test_fixture_group_can_be_applied_to_click_command(click_runner):
     @click.command()
     def command():
@@ -33,7 +17,7 @@ def test_fixture_group_can_be_applied_to_click_command(click_runner):
 
     grp = ConfigFixtureGroup()
 
-    @grp.fixture
+    @grp.fixture()
     def option():
         ...
 
