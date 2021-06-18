fe8087b67da7775e75e1f33b244ed9bdc8e2e3cb
Blake Smith <blakeinvictoria@gmail.com>
2021-06-18 10:21:10 -0700

    Add tests for new reads fixture.

---
+++ tests/virtool_workflow/analysis/test_analysis.py
@@ -0,0 +1,68 @@
+from pathlib import Path
+
+import pytest
+
+from tests.virtool_workflow.api.mocks.mock_sample_routes import TEST_SAMPLE_ID
+from virtool_workflow.abc.caches.analysis_caches import ReadsCache
+from virtool_workflow.analysis import reads
+from virtool_workflow.caching.local import LocalCaches
+from virtool_workflow.data_model import Job
+
+
+@pytest.fixture
+def scope(runtime):
+    runtime.add_provider(reads.fixtures)
+
+    runtime["job"] = Job(
+        id=test_get_reads_from_existing_cache.__name__,
+        args={
+            "sample_id": TEST_SAMPLE_ID,
+        }
+    )
+
+    return runtime
+
+
+async def test_get_reads_from_existing_cache(
+        scope,
+        tmpdir,
+        run_in_executor,
+        analysis_files,
+):
+
+    read_files = (analysis_files/"reads_1.fq.gz",
+                  analysis_files/"reads_2.fq.gz")
+    cache_path = Path(tmpdir)
+
+    caches = LocalCaches[ReadsCache](
+        path=cache_path,
+        run_in_executor=run_in_executor
+    )
+
+    cache_key = await scope.get_or_instantiate("trimming_cache_key")
+
+    async with caches.create(cache_key) as cache:
+        await cache.upload(read_files[0])
+        await cache.upload(read_files[1])
+
+        cache.quality = {}
+
+    scope["sample_caches"] = caches
+
+    trimmed_reads = await scope.instantiate(reads.reads)
+
+    assert trimmed_reads.sample.id == "hl5v0i0y"
+    assert trimmed_reads.quality == {}
+    assert trimmed_reads.left == cache_path/"reads_1.fq.gz"
+    assert trimmed_reads.right == cache_path/"reads_2.fq.gz"
+
+
+async def test_get_reads_without_cache(
+        scope,
+        data_regression,
+        file_regression
+):
+    trimmed_reads = await scope.instantiate(reads.reads)
+    assert trimmed_reads.sample.id == "hl5v0i0y"
+
+    assert data_regression.check(trimmed_reads.quality)
