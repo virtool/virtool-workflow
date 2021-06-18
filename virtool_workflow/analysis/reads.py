f0cf0aac324b16104348f3855b07eceab1b3c803
Blake Smith <blakeinvictoria@gmail.com>
2021-06-17 17:28:24 -0700

    Refactor `reads` fixture to perform trimming and caching.

--- virtool_workflow/analysis/reads.py
+++ virtool_workflow/analysis/reads.py
@@ -1,33 +1,18 @@
 from dataclasses import dataclass
-
-from virtool_workflow import fixture
-from virtool_workflow.abc.caches.analysis_caches import ReadsCache
-from virtool_workflow.analysis.utils import ReadPaths, make_read_paths
-from virtool_workflow.data_model import Sample
+from virtool_workflow.data_model.samples import Sample
+from pathlib import Path
 
 
 @dataclass
 class Reads:
     """The prepared reads for an analysis workflow."""
-    paired: bool
-    min_length: int
-    max_length: int
-    count: int
-    paths: ReadPaths
+    sample: Sample
+    path: Path
 
+    @property
+    def left(self):
+        return self.path / "reads_1.fq.gz"
 
-@fixture
-async def reads(
-    sample: Sample,
-    reads_cache: ReadsCache = None
-):
-    """A fixture for accessing trimmed reads for the current sample."""
-    min_length, max_length = reads_cache.quality["length"]
-
-    return Reads(
-        sample.paired,
-        min_length,
-        max_length,
-        reads_cache.quality["count"],
-        make_read_paths(reads_cache.path, sample.paired)
-    )
+    @property
+    def right(self):
+        return self.path / "reads_2.fq.gz"
--- virtool_workflow/analysis/trimming.py
+++ virtool_workflow/analysis/trimming.py
@@ -1,11 +1,34 @@
 """Calculate trimming parameters which are passed the Skewer read trimming tool."""
-from typing import Dict, Union
+import json
+import hashlib
+from pathlib import Path
+from typing import Dict, Tuple, Union
+from aiohttp import ClientSession
 
 from virtool_core.samples.utils import TRIM_PARAMETERS
 
+from virtool_workflow.analysis.fastqc import fastqc
 from virtool_workflow.analysis.library_types import LibraryType
+from virtool_workflow.analysis.sample import Sample
+from virtool_workflow.analysis.skewer import (calculate_trimming_min_length,
+                                              skewer)
+from virtool_workflow.fixtures.providers import FixtureGroup
+from virtool_workflow.api.caches import RemoteReadCaches
+from virtool_workflow.analysis.reads import Reads
 
 
+fixtures = FixtureGroup()
+
+
+@fixtures.fixture
+def trimming_min_length(sample: Sample):
+    return calculate_trimming_min_length(
+        sample.library_type,
+        sample.max_length
+    )
+
+
+@fixtures.fixture
 def trimming_parameters(
         library_type: LibraryType,
         trimming_min_length: int
@@ -36,3 +59,72 @@ def trimming_parameters(
         **TRIM_PARAMETERS,
         "min_length": trimming_min_length
     }
+
+
+@fixtures.fixture
+def trimming_cache_key(sample: Sample, trimming_parameters: dict):
+    """Compute a unique cache key based on the trimming parameters"""
+    trim_param_json = json.dumps({
+        "id": sample.id,
+        "min_length": sample.min_length,
+        **trimming_parameters,
+    }, sort_keys=True)
+
+    raw_key = "reads-" + trim_param_json
+
+    return hashlib.sha256(raw_key.encode()).hexdigest()
+
+
+@fixtures.fixture
+def sample_caches(
+        sample: Sample,
+        cache_path: Path,
+        jobs_api_url: str,
+        http: ClientSession,
+        run_in_executor,
+):
+    return RemoteReadCaches(
+        sample.id,
+        sample.paired,
+        cache_path,
+        http,
+        jobs_api_url,
+        run_in_executor
+    )
+
+
+@fixtures.fixture
+def reads(
+    sample: Sample,
+    sample_caches: RemoteReadCaches,
+    trimming_min_length: int,
+    trimming_parameters: dict,
+    trimming_cache_key: str,
+    work_path: Path,
+    run_subprocess,
+    run_in_executor
+):
+    """
+    The trimmed sample reads.
+
+    If a cache exists it will be used, otherwise a new cache will be created.
+    """
+
+    try:
+        cache = await sample_caches.get(trimming_cache_key)
+        return Reads(sample, cache.path)
+    except KeyError:
+        result = await skewer(
+            min_length=trimming_min_length,
+            **trimming_parameters
+        )(sample.read_paths, run_subprocess, run_in_executor)
+
+        quality = await fastqc(work_path, run_subprocess)(sample.read_paths)
+
+        async with sample_caches.create(trimming_cache_key) as cache:
+            for path in result.read_paths:
+                await cache.upload(path)
+
+                cache.quality = quality
+
+        return Reads(sample, result.read_paths)
