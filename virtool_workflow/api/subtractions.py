c9ef71deb45ef9a2b32eed0bd7f77c99c3f6e50f
Ian Boyes <igboyes@gmail.com>
2021-06-24 08:27:22 -0700

    Remove is_host from Subtraction data model
    The concept of host vs non-host subtractions is dead in Virtool and will be fully removed from the application. It is not used in workflows.

--- virtool_workflow/api/subtractions.py
+++ virtool_workflow/api/subtractions.py
@@ -18,9 +18,7 @@ def subtraction_from_json(subtraction_json: dict, path: Path) -> Subtraction:
         subtraction_json["name"],
         subtraction_json["nickname"],
         subtraction_json["count"] if "count" in subtraction_json else None,
-        NucleotideComposition(
-            **subtraction_json["gc"]) if "gc" in subtraction_json else {},
-        subtraction_json["is_host"],
+        NucleotideComposition(**subtraction_json["gc"]) if "gc" in subtraction_json else {},
         path,
     )
 
