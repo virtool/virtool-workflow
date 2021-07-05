80f1ecdc6f59fe8e45c09f8565710f2a589d7239
Blake Smith <blakeinvictoria@gmail.com>
2021-07-05 10:19:33 -0700

    Remove `load_fixtures_from__fixtures__`.
    This functionality is no longer used, in favor of explicit fixture
    imports.

--- virtool_workflow/abc/runtime/runtime.py
+++ virtool_workflow/abc/runtime/runtime.py
@@ -1,16 +1,10 @@
 from abc import abstractmethod, ABC
 from typing import Dict, Any
 
-from virtool_workflow.discovery import load_fixture_plugins
 from virtool_workflow.workflow import Workflow
 
 
 class AbstractWorkflowEnvironment(ABC):
-
-    @staticmethod
-    def load_plugins(*plugin_modules: str):
-        """Load fixtures from plugins."""
-        return load_fixture_plugins(plugin_modules)
 
     @abstractmethod
     async def execute(self, workflow: Workflow) -> Dict[str, Any]:
