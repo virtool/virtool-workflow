6954767eed98d18d82a07c32712833b75fcb424c
Blake Smith <blakeinvictoria@gmail.com>
2021-07-07 12:47:28 -0700

    Fix branch name condition.

--- .github/act/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Run `act` to test github actions
-
-# Test that `poetry publish` dry run succeeds
-act -s PYPI_USERNAME="invalid" -s PYPI_TOKEN="invalid" -j pypi-check
-
-act -j pypi-publish
