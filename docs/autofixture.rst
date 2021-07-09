c7d94f17792ee21d745b6f1caba5b605b7abdf0c
Blake Smith <blakeinvictoria@gmail.com>
2021-07-09 14:29:58 -0700

    Add example usage for `.. autofixture`

---
+++ docs/autofixture.rst
@@ -0,0 +1,10 @@
+``autofixture``
+=================
+
+A custom sphinx extension for rendering fixture documentation.
+
+
+.. autofixture:: showcase.protocol_fixture
+
+
+.. autofixture:: showcase.regular_fixture
---
+++ docs/showcase.py
@@ -0,0 +1,28 @@
+from typing import Protocol
+from virtool_workflow import fixture
+from typing import Any
+
+class ReturnProtocol(Protocol):
+    """A showcase return protocol"""
+
+    def __call__(self, a: int, b: str, c: Any, d: str = "default") -> Any:
+        """
+        A function that does something...
+
+        :param a: An argument to the function.
+        :param b: An argument to the function.
+        :param c: An argument to the function.
+
+        :return: Anything, but probably nothing.
+        :raises NotImplementedError: Every time.
+        """
+
+@fixture
+def regular_fixture(a, b, c) -> int:
+    """An example fixture."""
+    ...
+
+
+@fixture(protocol=ReturnProtocol)
+def protocol_fixture(regular_fixture) -> ReturnProtocol:
+    ...
