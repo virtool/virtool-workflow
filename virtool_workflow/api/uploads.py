874d76c2bb44a697686be5feae3d31110f26e6ff
Blake Smith <blakeinvictoria@gmail.com>
2021-07-12 11:11:15 -0700

    Add `input_files` fixture.
    Downloads the input files and provides a dict mapping file names to Path.

---
+++ virtool_workflow/api/uploads.py
@@ -0,0 +1,63 @@
+import asyncio
+from typing import Protocol, Dict, List
+from pathlib import Path
+from virtool_workflow import fixture
+from virtool_workflow.api.utils import read_file_from_response
+from virtool_core.data_model.job import Job
+
+
+class FileDownloader(Protocol):
+    async def __call__(self, id: str, target: Path) -> Path:
+        """
+        Download a file.
+
+        :param id: The unique ID fo the file.
+        :type id: str
+        :param target: The path to which the file is downloaded.
+        :type target: Path
+
+        :returns: The target path, now locating the downloaded file.
+        :rtype: Path
+        """
+
+
+@fixture(protocol=FileDownloader)
+def download_input_file(http, jobs_api_url: str) -> FileDownloader:
+    """Download files from Virtool's uploads API."""
+    async def download(id, target):
+        target_url = f"{jobs_api_url}/uploads/{id}"
+        async with http.get(target_url) as response:
+            return await read_file_from_response(response, target)
+
+    return download
+
+
+@fixture
+def files_list(job: Job) -> List[dict]:
+    """The files dictionary for the current job."""
+    return job.args["files"]
+
+            
+@fixture
+async def input_files(
+    files_list: List[dict],
+    download_input_file: FileDownloader, 
+    work_path: Path
+) -> Dict[str, Path]:
+    """
+    The downloaded input files for the current workflow run. 
+
+    :returns: A dictionary mapping file names to their locations on disk.
+    """
+    target_dir = work_path/"files"
+    target_dir.mkdir()
+
+    downloads = await asyncio.gather(
+        [
+            download_input_file(f["id"], target_dir/f["name"]) 
+            for f in files_list
+        ],
+        return_exceptions=True
+    )
+
+    return {f["name"]: path for f, path in zip(files_list, downloads)}
