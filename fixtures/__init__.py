b766001babdf87cf731998f500e97fab99e6c36a
Blake Smith <blakeinvictoria@gmail.com>
2021-07-14 23:34:55 -0700

    Add `@fixture` decorator.

--- fixtures/__init__.py
+++ fixtures/__init__.py
@@ -1,9 +1,10 @@
 """Pytest style fixtures for use in python applications."""
-from ._fixture import (Fixture, FixtureValue, fixture_context, get_fixtures,
-                       runs_in_new_fixture_context)
+from ._fixture import (Fixture, FixtureValue, fixture, fixture_context,
+                       get_fixtures, runs_in_new_fixture_context)
 
 __all__ = [
     "fixture_context",
+    "fixture",
     "Fixture",
     "FixtureValue",
     "get_fixtures",
