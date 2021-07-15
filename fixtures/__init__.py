f084f9dea731445deb092e85aee00e00d1eaea61
Blake Smith <blakeinvictoria@gmail.com>
2021-07-14 22:20:34 -0700

    Add decorator to run a function in a new fixture context.

--- fixtures/__init__.py
+++ fixtures/__init__.py
@@ -1,9 +1,11 @@
 """Pytest style fixtures for use in python applications."""
-from ._fixture import Fixture, FixtureValue, fixture_context, get_fixtures
+from ._fixture import (Fixture, FixtureValue, fixture_context, get_fixtures,
+                       runs_in_new_fixture_context)
 
 __all__ = [
     "fixture_context",
     "Fixture",
     "FixtureValue",
     "get_fixtures",
+    "runs_in_new_fixture_context",
 ]
