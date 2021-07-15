1efee43fedf59d83238bc412f1fc13af4262b55d
Blake Smith <blakeinvictoria@gmail.com>
2021-07-15 03:36:40 -0700

    Implement fixture binding and instantiation.

--- fixtures/_scope.py
+++ fixtures/_scope.py
@@ -1,9 +1,13 @@
+
+import asyncio
 import inspect
 from collections import UserDict
-from contextlib import AsyncExitStack, asynccontextmanager, contextmanager
-from typing import Union
+from contextlib import AsyncExitStack, asynccontextmanager, contextmanager, suppress
+from functools import wraps
+from typing import Any, AnyStr, Dict, Union
 
-from ._fixture import Fixture
+from ._fixture import Fixture, get_fixtures
+from .errors import FixtureBindingError, FixtureNotFound
 
 
 class FixtureScope(UserDict):
@@ -75,9 +79,191 @@ class FixtureScope(UserDict):
         return function(*args, **kwargs)
 
 
+    @property
+    def available(self) -> Dict[str, Any]:
+        """A dict of all available fixtures."""
+        fixtures = get_fixtures()
+        return dict(fixtures, **self)
 
-    async def instantiate(self, function: Union[callable, Fixture]):
-        ...
+
+    @staticmethod
+    def _get_arg_spec(function, follow_wrapped=False) -> inspect.FullArgSpec:
+        """
+        Get the arg spec for a function.
+
+        :param function: A function.
+        :param follow_wrapped: Follow `__wrapped__`, defaults to False.
+
+        :return: A :class:`inspect.FullArgSpec`
+        """
+        if follow_wrapped:
+            function = inspect.unwrap(function)
+        return inspect.getfullargspec(function)
+
+
+    @staticmethod
+    def _make_future(value: Any) -> asyncio.Future:
+        """Create a future from a variable."""
+        future = asyncio.Future()
+        future.set_result(value)
+        return future
+
+
+    async def bind(
+        self,
+        function: Union[callable, Fixture],
+        follow_wrapped: bool = False,
+        **kwargs: Any,
+    ) -> Union[callable, Fixture]:
+        """
+        Instantiate fixtures as needed based on the signature of
+        `function`. Wrap `function` to pass the fixture values as 
+        parameters where needed.
+
+        This function will try to get a value for each key with the following priority:
+
+            :first: 
+                Values from `**kwargs`. if one is present for the given key then 
+                the corresponding fixture would not be instantiated.
+            
+            :second:
+                If no value is present in `**kwargs` the fixture will be instantiated
+                and it's return value will be used.
+
+            :third:
+                Lastly, if the fixture cannot be instantiated, the default value for the
+                parameter (on the fixture function) will be used if it exists.
+
+        :param function: The function to bind fixtures to.
+        :param follow_wrapped: Use the signature of the wrapped function
+                               instead of the outer function, defaults to False
+        :param kwargs: Additional keyword arguments to bind to the function.
+                       This will be used instead of the fixture value when there
+                       is a name collision.
+        :raise FixtureBindingError: When a required fixture cannot be found or instantiated.
+
+        :return: A function wrapping `function` which does not require arguments.
+                 If arguments are given they will overwrite the bound fixture values.
+        """
+        argspec = self._get_arg_spec(function, follow_wrapped)
+
+        if argspec.varargs is not None or argspec.varkw is not None:
+            raise FixtureBindingError(
+                function, argspec.varargs,
+                reason="Binding fixtures to `*args` or `**kwargs` is not supported."
+            )
+
+        if len(argspec.args) == 0:
+            return function
+
+        if argspec.defaults:
+            args_with_default = argspec.args[-len(argspec.defaults):]
+            defaults = {key: value for key, value in zip(args_with_default, argspec.defaults)}
+        else:
+            defaults = {}
+
+        keywordargs = {}
+        for key in argspec.args:
+            with suppress(KeyError):
+                keywordargs[key] = kwargs[key]
+                continue
+
+            try:
+                keywordargs[key] = await self.instantiate_by_key(key)
+            except Exception as e:
+                if key in defaults:
+                    keywordargs[key] = defaults[key]
+                    continue
+
+                raise FixtureBindingError(
+                    function, key, 
+                    reason=f"Could not instantiate `{key}` due to {e.__class__.__name__}"
+                ) from e
+
+
+        if inspect.iscoroutinefunction(function):
+            @wraps(function)
+            async def _bound(*args, **kwargs):
+                keywordargs.update(kwargs)
+                return await function(*args, **keywordargs)
+        else:
+            @wraps(function)
+            def _bound(*args, **kwargs):
+                if args or kwargs:
+                    # Since `keywordargs` is stored in the closure
+                    # updates to it will persist between calls
+                    _keywordargs = keywordargs.copy()
+                else:
+                    _keywordargs = keywordargs
+
+                # Support using keyword arguments when calling `_bound`
+                _keywordargs.update(kwargs)
+
+                # Support using positional arguments when calling `_bound`
+                if args:
+                    # Remove supplied args from the `keywordargs`
+                    arg_names = argspec.args[:len(args)]
+                    for name in arg_names:
+                        del _keywordargs[name]
+                return function(*args, **_keywordargs)
+
+        return _bound
+
+
+    async def instantiate_by_key(self, key: str, *args, **kwargs) -> Any:
+        """Instantiate a fixture by it's name."""
+        with suppress(KeyError):
+            return self[key]
+
+        fixtures = get_fixtures()
+        try:
+            fixture = fixtures[key]
+        except KeyError: 
+            raise FixtureNotFound(key, self)
+
+        return await self.instantiate(fixture, *args, **kwargs)
+
+
+
+    async def get_or_instantiate(
+        self,
+        fixture: Union[str, callable, Fixture],
+        *args: Any, 
+        **kwargs: Any, 
+    ):
+        """Instantiate a fixture by name, or directly."""
+        if isinstance(fixture, AnyStr):
+            return self.instantiate_by_key(fixture, *args, **kwargs)
+        else:
+            return self.instantiate(fixture, *args, **kwargs)
+
+
+    async def instantiate(self, fixture: Fixture, *args, **kwargs):
+        """
+        Instantiate a given fixture and add it to the scope.
+
+        Recursively bind parameters of the function as required.
+
+        :param function: The function/fixture to instantiate.
+        :return: The fixture's value.
+        """
+        with suppress(KeyError):
+            return self[fixture.__name__]
+
+        try:
+            value = await self._instantiate(fixture, *args, **kwargs)
+        except TypeError as e:
+            if "missing" not in str(e):
+                raise
+
+            value = await self.instantiate(await self.bind(fixture, follow_wrapped=True), *args, **kwargs)
+
+
+        self[fixture.__name__] = value
+
+        return value
+
+
 
 
 
