5d5f9f3276062180e85670bba0a2e0019e765798
Blake Smith <blakeinvictoria@gmail.com>
2021-07-14 22:09:43 -0700

    Use contextvars to store fixtures.
    This is perferred over a global variable for the following reasons:
    
    1. A different dictionary can be used in different concurrent tasks.
    
    2. The same dictionary will be used when desired.
    
    3. Fine-grained control over what fixtures are available in any given block.
    
    4. Separate dictionaries for each test case.

--- fixtures/__init__.py
+++ fixtures/__init__.py
@@ -1,1 +1,9 @@
 """Pytest style fixtures for use in python applications."""
+from ._fixture import Fixture, FixtureValue, fixture_context, get_fixtures
+
+__all__ = [
+    "fixture_context",
+    "Fixture",
+    "FixtureValue",
+    "get_fixtures",
+]
--- fixtures/_fixture.py
+++ fixtures/_fixture.py
@@ -1,4 +1,8 @@
-from typing import Any, NewType, Protocol, runtime_checkable
+import inspect
+from functools import wraps
+from contextlib import contextmanager
+from contextvars import ContextVar
+from typing import Any, Dict, NewType, Protocol, runtime_checkable
 
 FixtureValue = NewType('FixtureValue', Any)
 """A return value from a :class:`Fixture` callable."""
@@ -13,7 +17,70 @@ class Fixture(Protocol):
     Enables `isinstance(function, Fixture)` checks to be made.
 
     """
+    __name__: str
     __is_fixture__: bool = True
 
     async def __call__(self, *args, **kwargs) -> FixtureValue:
         ...
+
+
+_fixtures = ContextVar("fixtures")
+
+
+def get_fixtures():
+    """get the fixtures dictionary for the current context."""
+    try:
+        return _fixtures.get()
+    except LookupError:
+        _fixtures.set({})
+        return _fixtures.get()
+
+
+@contextmanager
+def fixture_context(*fixtures: Fixture, copy_context=True) -> Dict[str, Fixture]:
+    """
+    Enter a new fixture context.
+
+    Any fixtures added within the `with` block will no not be available outside of it.
+
+    :param fixtures: Any fixtures which should be included in the context.
+                     If a fixture with the same name is already present, the 
+                     new fixture will be used instead.
+    :param copy_context: Copy the fixtures dict from the current context, defaults to True.
+                     
+
+    :yield: A dictionary mapping fixture names to :class:`Fixture` functions.
+    """
+    if copy_context is True:
+        current_fixtures = get_fixtures()
+        _fixtures_copy = current_fixtures.copy()
+    else:
+        _fixtures_copy = {}
+
+    for fixture in fixtures:
+        _fixtures_copy[fixture.__name__] = fixture
+
+    token = _fixtures.set(_fixtures_copy)
+
+    try:
+        yield _fixtures.get()
+    finally:
+        _fixtures.reset(token)
+
+
+def runs_in_new_fixture_context(function: callable):
+    """Decorator which causes the function to be executed in a new fixture context."""
+    if inspect.iscoroutinefunction(function):
+        @wraps(function)
+        async def _in_new_context(*args, **kwargs):
+            with fixture_context():
+                return await function(*args, **kwargs)
+    else:
+        @wraps(function)
+        def _in_new_context(*args, **kwargs):
+            with fixture_context():
+                return function(*args, **kwargs)
+
+    return _in_new_context
+
+
