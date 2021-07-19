ce98710f3b9662fb5b566f1b52b32c395b735da5
Blake Smith <blakeinvictoria@gmail.com>
2021-07-19 10:26:57 -0700

    Simplify `.bind()`.

--- fixtures/_scope.py
+++ fixtures/_scope.py
@@ -1,12 +1,13 @@
 
-import asyncio
 import inspect
 from collections import UserDict
-from contextlib import AsyncExitStack, asynccontextmanager, contextmanager, suppress
+from contextlib import (AsyncExitStack, asynccontextmanager, contextmanager,
+                        suppress)
 from functools import wraps
 from typing import Any, AnyStr, Dict, Union
 
 from ._fixture import Fixture, get_fixtures
+from ._utils import get_arg_spec, get_defaults
 from .errors import FixtureBindingError, FixtureNotFound
 
 
@@ -28,12 +29,14 @@ class FixtureScope(UserDict):
         self.exit_stack = await AsyncExitStack().__aenter__()
         return self
 
-
     async def __aexit__(self, *args):
         """
-        Close this scope, ensuring that all generator and async generator
-        fixtures are completed.
+        Close the :class:FixtureScope.
+
+            1. Remove all references to fixture values.
+            2. Close the exit stack, ensuring that generator fixtures are closed out.
         """
+        self.clear()
         await self.exit_stack.__aexit__(*args)
 
     async def _instantiate(self, function: Union[callable, Fixture], *args, **kwargs):
@@ -57,19 +60,20 @@ class FixtureScope(UserDict):
         :return: The return (or yield) value of `function`.
         """
         if inspect.isasyncgenfunction(function):
-            ctx_manager = asynccontextmanager(function)
-            return await self.exit_stack.enter_async_context(ctx_manager(*args, **kwargs))
+            return await self.exit_stack.enter_async_context(
+                asynccontextmanager(function)(*args, **kwargs)
+            )
 
         if inspect.isgeneratorfunction(function):
-            ctx_manager = contextmanager(function)
-            return self.exit_stack.enter_context(ctx_manager(*args, **kwargs))
-        
+            return self.exit_stack.enter_context(
+                contextmanager(function)(*args, **kwargs)
+            )
+
         if inspect.iscoroutinefunction(function):
             return await function(*args, **kwargs)
 
-        # Must be a synchronous function.
+        # Must be a synchronous function..
         return function(*args, **kwargs)
-
 
     @property
     def available(self) -> Dict[str, Any]:
@@ -77,29 +81,49 @@ class FixtureScope(UserDict):
         fixtures = get_fixtures()
         return dict(fixtures, **self)
 
-
-    @staticmethod
-    def _get_arg_spec(function, follow_wrapped=False) -> inspect.FullArgSpec:
+    def _bind(
+        self,
+        function: callable,
+        argspec: inspect.FullArgSpec,
+        values: dict,
+    ) -> callable:
         """
-        Get the arg spec for a function.
+        Bind values to function parameters.
 
-        :param function: A function.
-        :param follow_wrapped: Follow `__wrapped__`, defaults to False.
+        :param function: The function to bind.
+        :param argspec: The argspec for the function, obtained 
+                        via :func:`inspect.getfullargspec`.
+        :param values: Key value pairs to bind to the function.
 
-        :return: A :class:`inspect.FullArgSpec`
+        :return: The bound function.
         """
-        if follow_wrapped:
-            function = inspect.unwrap(function)
-        return inspect.getfullargspec(function)
+        if inspect.iscoroutinefunction(function):
+            @wraps(function)
+            async def _bound(*args, **kwargs):
+                values.update(kwargs)
+                return await function(*args, **values)
+        else:
+            @wraps(function)
+            def _bound(*args, **kwargs):
+                if args or kwargs:
+                    # Since `values` is stored in the closure
+                    # updates to it will persist between calls
+                    _values = values.copy()
+                else:
+                    _values = values
 
+                # Support using keyword arguments when calling `_bound`
+                _values.update(kwargs)
 
-    @staticmethod
-    def _make_future(value: Any) -> asyncio.Future:
-        """Create a future from a variable."""
-        future = asyncio.Future()
-        future.set_result(value)
-        return future
+                # Support using positional arguments when calling `_bound`
+                if args:
+                    # Remove supplied args from the `values`
+                    arg_names = argspec.args[:len(args)]
+                    for name in arg_names:
+                        del _values[name]
+                return function(*args, **_values)
 
+        return _bound
 
     async def bind(
         self,
@@ -117,13 +141,13 @@ class FixtureScope(UserDict):
             :first: 
                 Values from `**kwargs`. if one is present for the given key then 
                 the corresponding fixture would not be instantiated.
-            
+
             :second:
                 If no value is present in `**kwargs` the fixture will be instantiated
                 and it's return value will be used.
 
             :third:
-                Lastly, if the fixture cannot be instantiated, the default value for the
+                If the fixture cannot be instantiated, the default value for the
                 parameter (on the fixture function) will be used if it exists.
 
         :param function: The function to bind fixtures to.
@@ -137,7 +161,7 @@ class FixtureScope(UserDict):
         :return: A function wrapping `function` which does not require arguments.
                  If arguments are given they will overwrite the bound fixture values.
         """
-        argspec = self._get_arg_spec(function, follow_wrapped)
+        argspec = get_arg_spec(function, follow_wrapped)
 
         if argspec.varargs is not None or argspec.varkw is not None:
             raise FixtureBindingError(
@@ -148,59 +172,25 @@ class FixtureScope(UserDict):
         if len(argspec.args) == 0:
             return function
 
-        if argspec.defaults:
-            args_with_default = argspec.args[-len(argspec.defaults):]
-            defaults = {key: value for key, value in zip(args_with_default, argspec.defaults)}
-        else:
-            defaults = {}
+        defaults = get_defaults(argspec)
 
-        keywordargs = {}
         for key in argspec.args:
-            with suppress(KeyError):
-                keywordargs[key] = kwargs[key]
+            if key in kwargs:
                 continue
 
             try:
-                keywordargs[key] = await self.instantiate_by_key(key)
+                kwargs[key] = await self.instantiate_by_key(key)
             except Exception as e:
                 if key in defaults:
-                    keywordargs[key] = defaults[key]
+                    kwargs[key] = defaults[key]
                     continue
 
                 raise FixtureBindingError(
-                    function, key, 
+                    function, key,
                     reason=f"Could not instantiate `{key}` due to {e.__class__.__name__}"
                 ) from e
 
-
-        if inspect.iscoroutinefunction(function):
-            @wraps(function)
-            async def _bound(*args, **kwargs):
-                keywordargs.update(kwargs)
-                return await function(*args, **keywordargs)
-        else:
-            @wraps(function)
-            def _bound(*args, **kwargs):
-                if args or kwargs:
-                    # Since `keywordargs` is stored in the closure
-                    # updates to it will persist between calls
-                    _keywordargs = keywordargs.copy()
-                else:
-                    _keywordargs = keywordargs
-
-                # Support using keyword arguments when calling `_bound`
-                _keywordargs.update(kwargs)
-
-                # Support using positional arguments when calling `_bound`
-                if args:
-                    # Remove supplied args from the `keywordargs`
-                    arg_names = argspec.args[:len(args)]
-                    for name in arg_names:
-                        del _keywordargs[name]
-                return function(*args, **_keywordargs)
-
-        return _bound
-
+        return self._bind(function, argspec, kwargs)
 
     async def instantiate_by_key(self, key: str, *args, **kwargs) -> Any:
         """Instantiate a fixture by it's name."""
@@ -210,25 +200,22 @@ class FixtureScope(UserDict):
         fixtures = get_fixtures()
         try:
             fixture = fixtures[key]
-        except KeyError: 
+        except KeyError:
             raise FixtureNotFound(key, self)
 
         return await self.instantiate(fixture, *args, **kwargs)
 
-
-
     async def get_or_instantiate(
         self,
         fixture: Union[str, callable, Fixture],
-        *args: Any, 
-        **kwargs: Any, 
+        *args: Any,
+        **kwargs: Any,
     ):
         """Instantiate a fixture by name, or directly."""
         if isinstance(fixture, AnyStr):
             return self.instantiate_by_key(fixture, *args, **kwargs)
         else:
             return self.instantiate(fixture, *args, **kwargs)
-
 
     async def instantiate(self, fixture: Fixture, *args, **kwargs):
         """
@@ -250,13 +237,6 @@ class FixtureScope(UserDict):
 
             value = await self.instantiate(await self.bind(fixture, follow_wrapped=True), *args, **kwargs)
 
-
         self[fixture.__name__] = value
 
         return value
-
-
-
-
-
-
