2dd60940a58e660e005c7e58edcdc7f119656d27
Blake Smith <blakeinvictoria@gmail.com>
2021-07-20 13:16:04 -0700

    Use regular click options instead of config fixtures.
    create a single `config` fixture with values from the CLI args.

---
+++ virtool_workflow/options.py
@@ -0,0 +1,65 @@
+from typing import List
+
+import click
+
+options = [
+    click.argument("job_id"),
+    click.option(
+        "--work-path",
+        default="temp",
+        help="The path where temporary files will be stored.",
+        type=click.Path()
+    ),
+    click.option(
+        "--proc",
+        help="The number of processes to use.",
+        type=int,
+        default=2,
+    ),
+    click.option(
+        "--mem",
+        help="The amount of memory to use in GB.",
+        type=int,
+        default=8,
+    ),
+    click.option(
+        "--dev-mode",
+        help="Run in development mode.",
+        is_flag=True,
+    ),
+    click.option(
+        "--jobs-api-url",
+        help="The URL of the jobs API.",
+        default="https://localhost:9950/api",
+    ),
+    click.option(
+        "--is-analysis-workflow",
+        help="The workflow is an analysis workflow.",
+        is_flag=True
+    ),
+    click.option(
+        "--workflow-file-path",
+        type=click.Path(exists=True),
+        default="workflow.py",
+        help="The path to the workflow file.",
+    ),
+    click.option(
+        "--init-file",
+        help="The path to the init file.",
+        type=click.Path(exists=True),
+        default="init.py"
+    ),
+    click.option(
+        "--fixtures-file",
+        help="The path to the fixtures file.",
+        type=click.Path(exists=True),
+        default="fixtures.py"
+    ),
+]
+
+
+def apply_options(func, options: List[click.Option] = options):
+    """Apply click options from a list."""
+    for option in options:
+        func = option(func)
+    return func
