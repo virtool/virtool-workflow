77252faee99f03297a346a7b71ee950b87ac4ace
Blake Smith <blakeinvictoria@gmail.com>
2021-07-20 13:46:48 -0700

    Update `autofixture` plugin to use new fixtures module.

--- docs/_ext/autofixture.py
+++ docs/_ext/autofixture.py
@@ -1,5 +1,6 @@
 import inspect
 import re
+import fixtures
 from typing import Any
 
 from docutils.parsers.rst import directives
@@ -28,6 +29,7 @@ class FixtureDirective(PyFunction):
     def needs_arglist(self) -> bool:
         return True
 
+
 class FixtureDocumenter(FunctionDocumenter):
     objtype = "fixture"
     directivetype = "fixture"
@@ -37,21 +39,19 @@ class FixtureDocumenter(FunctionDocumenter):
     def can_document_member(cls,
                             member: Any, membername: str,
                             isattr: bool, parent: Any) -> bool:
-        return hasattr(member, 'isfixture')
+        return isinstance(member, fixtures.Fixture)
 
-    
     def format_args(self, **kwargs: Any) -> str:
         args = super(FixtureDocumenter, self).format_args(**kwargs)
 
-        if hasattr(self.object, "__hide_params__"):
-            if self.object.__hide_params__:
-                args = re.sub(
-                    "\(.*?\)",
-                    "(...)",
-                    args
-                )
+        if self.object.__hide_params__:
+            args = re.sub(
+                "\(.*?\)",
+                "(...)",
+                args
+            )
 
-        if hasattr(self.object, "__return_protocol__"):
+        if self.object.__return_protocol__ is not None:
             if "->" not in args:
                 args = args + " -> "
 
@@ -63,19 +63,18 @@ class FixtureDocumenter(FunctionDocumenter):
             args = re.sub(
                 "->.*$",
                 f"-> {async_prefix}{inspect.signature(self.object.__return_protocol__.__call__)}",
-                 args
+                args
             )
 
         return args
 
-    
 
 def add_protocol_signatures(app, what, name, obj, options, lines):
     """Add the signature of the protocol __call__ to the docstring"""
     if what != "fixture":
         return
 
-    if hasattr(obj, "__return_protocol__"):
+    if obj.__return_protocol__ is not None:
         doc = inspect.getdoc(obj.__return_protocol__.__call__)
 
         lines.clear()
