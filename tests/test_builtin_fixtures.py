66ce5ed85934599b3627c459cdbe11cca15254a7
Blake Smith <blakeinvictoria@gmail.com>
2021-07-20 13:13:17 -0700

    Update tests.

---
+++ tests/test_builtin_fixtures.py
@@ -0,0 +1,15 @@
+from fixtures import runs_in_new_fixture_context, FixtureScope
+from virtool_workflow.builtin_fixtures import work_path
+from pathlib import Path
+
+
+@runs_in_new_fixture_context(work_path, copy_context=False)
+async def test_work_path():
+    async with FixtureScope() as scope:
+        scope["config"] = {"work_path": "temp"}
+        path = await scope.instantiate_by_key("work_path")
+
+        assert isinstance(path, Path)
+        assert path.exists
+
+    assert not path.exists()
