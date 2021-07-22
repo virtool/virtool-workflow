2fc1876a1e047dd1f21f3329ae4274d5c3daf6b0
Blake Smith <blakeinvictoria@gmail.com>
2021-07-22 15:52:00 -0700

    Update jobs.py

--- virtool_workflow/api/jobs.py
+++ virtool_workflow/api/jobs.py
@@ -41,7 +41,7 @@ async def acquire_job_by_id(
                 mem=document["mem"] if "mem" in document else mem,
                 proc=document["proc"] if "proc" in document else proc,
                 status=document["status"],
-                task=document["workflow"],
+                task=document["task"],
                 key=document["key"],
             )
 
