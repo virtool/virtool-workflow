7db44d6db3105c38b0b8d43fb41370bf2454ef7a
Blake Smith <blakeinvictoria@gmail.com>
2021-07-23 14:36:38 -0700

    Change `workflow test` to use YAML data.

--- virtool_workflow/testing/cli.py
+++ virtool_workflow/testing/cli.py
@@ -1,5 +1,7 @@
 import os
 import sys
+import click
+import yaml
 from pathlib import Path
 from shutil import which
 from subprocess import PIPE, Popen
@@ -11,12 +13,24 @@ WORKFLOW_COMPOSE_FILE = Path(__file__).parent/"docker-compose.workflow.yml"
 COMPOSE_FILE = f"{API_COMPOSE_FILE.absolute()}:{WORKFLOW_COMPOSE_FILE.absolute()}"
 
 
+@click.option(
+    "--job-id",
+    help="Job ID to use for the workflow run.",
+    default=None,
+)
 @apply_options
-def test_main(**kwargs):
+def test_main(job_id, **kwargs):
     """Run a workflow in a test environment"""
+
+    if job_id is None:
+        with open("test_case.yml", "r") as test_case_yml:
+            yml = yaml.safe_load(test_case_yml)
+            job_id = yml["job_id"]
 
     if which("docker-compose") is None:
         raise RuntimeError("docker-compose is not installed.")
+
+    run_args = [job_id] + sys.argv[2:]
 
     proc = Popen(
         [
@@ -31,7 +45,7 @@ def test_main(**kwargs):
         stdout=PIPE,
         stderr=PIPE,
         env={
-            "VT_ADD_ARGS": " ".join(sys.argv[2:]),
+            "VT_ADD_ARGS": " ".join(run_args),
             "COMPOSE_FILE": COMPOSE_FILE,
             "WORKFLOW_DIR": os.getcwd(),
             **os.environ,
