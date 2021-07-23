5534a725d4c22686154e30ba57bb554b4e935b33
Blake Smith <blakeinvictoria@gmail.com>
2021-07-23 15:12:02 -0700

    Update `run.sh`

--- integration_tests/run.sh
+++ integration_tests/run.sh
@@ -3,7 +3,7 @@
 set -e
 
 (cd .. && docker build -t virtool/workflow .)
-(cd indexes && workflow test --is-analysis-workflow index_integration_test_job)
-(cd analysis && workflow test --is-analysis-workflow analysis_integration_test_job)
-(cd samples && workflow test --is-analysis-workflow sample_integration_test_job)
-(cd subtractions && workflow test --is-analysis-workflow subtractions_integration_test_job)
+(cd indexes && workflow test --is-analysis-workflow)
+(cd analysis && workflow test --is-analysis-workflow)
+(cd samples && workflow test --is-analysis-workflow)
+(cd subtractions && workflow test --is-analysis-workflow)
