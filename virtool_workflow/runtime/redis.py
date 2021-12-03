cf6dcf5a249419fa807cb056c0e8d005b497a8ef
Ian Boyes <igboyes@gmail.com>
2021-12-03 13:17:29 -0800

    fix: use virtool-core redis connection
    * Use shared Redis logic from core.
    * Use ping function from core to keep connection alive.

--- virtool_workflow/runtime/redis.py
+++ virtool_workflow/runtime/redis.py
@@ -1,8 +1,12 @@
-import aioredis
-from .runtime import run_workflow, prepare_workflow
+from asyncio import create_task
+
+from aioredis import Redis
+from virtool_core.redis import connect_to_redis, periodically_ping_redis
+
+from .runtime import prepare_workflow, run_workflow
 
 
-async def redis_jobs(list_name: str, redis: aioredis.Redis) -> str:
+async def redis_jobs(list_name: str, redis: Redis) -> str:
     """An async generator yielding redis job IDs."""
     while True:
         _, _id = await redis.blpop(list_name)
@@ -15,9 +19,13 @@ async def run_jobs_from_redis(
     **config,
 ):
     """Run jobs from a redis list."""
-    redis = await aioredis.create_redis_pool(redis_url)
+    redis = await connect_to_redis(redis_url)
+    ping_task = create_task(periodically_ping_redis(redis))
 
     async with prepare_workflow(**config) as workflow:
         async for job_id in redis_jobs(list_name, redis):
             config["job_id"] = job_id
             await run_workflow(workflow, config)
+
+    ping_task.cancel()
+    await ping_task
