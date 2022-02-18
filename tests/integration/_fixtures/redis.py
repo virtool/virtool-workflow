a530d3e7bbe59cc742d72ba62ef123b4c7b5556c
Blake Smith <blakeinvictoria@gmail.com>
2022-02-18 12:49:33 -0800

    test: add redis fixture for pytest

---
+++ tests/integration/_fixtures/redis.py
@@ -0,0 +1,15 @@
+import pytest
+from virtool_core.redis import connect_to_redis
+
+
+@pytest.fixture
+async def redis(redis_service):
+    _redis = await connect_to_redis(redis_service)
+    try:
+        yield _redis
+    finally:
+        _redis.close()
+        await _redis.wait_closed()
+
+    
+
