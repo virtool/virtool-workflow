7253ec915b39c95c3fe310f69e827f69199f05ab
= <blakeinvictoria@gmail.com>
2022-05-26 09:24:59 -0700

    refactor: use workflow errors
    Use errors from virtool_workflow.errors more consistently.

--- virtool_workflow/errors.py
+++ virtool_workflow/errors.py
@@ -1,6 +1,10 @@
 class IllegalJobArguments(ValueError):
+    """The `job.args` dict is in an illegal state."""
+
     ...
 
 
 class MissingJobArgument(ValueError):
+    """The `job.args` dict is missing a required key for some funcionality."""
+
     ...
